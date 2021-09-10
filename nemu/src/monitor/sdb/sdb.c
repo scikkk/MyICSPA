a62c8ae2f375f7c3af882e18ce880f873de78b05
tracer-ics2021 <tracer@njuics.org>
2021-09-10 11:15:09 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d628f61378474fa788b7de345d293e65     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  11:15:09 up 54 min,  1 user,  load average: 0.09, 0.06, 0.04

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -11,33 +11,33 @@ void init_wp_pool();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
-  static char *line_read = NULL;
+	static char *line_read = NULL;
 
-  if (line_read) {
-    free(line_read);
-    line_read = NULL;
-  }
+	if (line_read) {
+		free(line_read);
+		line_read = NULL;
+	}
 
-  line_read = readline("(nemu) ");
+	line_read = readline("(nemu) ");
 
-  if (line_read && *line_read) {
-    add_history(line_read);
-  }
+	if (line_read && *line_read) {
+		add_history(line_read);
+	}
 
-  return line_read;
+	return line_read;
 }
 
 static int cmd_c(char *args) {
-  cpu_exec(-1);
-  return 0;
+	cpu_exec(-1);
+	return 0;
 }
 
 
 static int cmd_q(char *args) {
-  // wk add	
-  nemu_state.state = NEMU_QUIT;
-  // wk add
-  return -1;
+	// wk add	
+	nemu_state.state = NEMU_QUIT;
+	// wk add
+	return -1;
 
 }
 
@@ -46,99 +46,101 @@ static int cmd_help(char *args);
 
 // added by wk: execute by step
 static int cmd_si(char *args) {
-  int len_of_step = atoi(strtok(NULL, " "));
-  cpu_exec(len_of_step);
-  return 0;
+	char* len_of_step_str = strtok(NULL, " ");
+	int len_of_step = 1;
+	if (len_of_step_str != NULL) {len_of_step = atoi(len_of_step_str);}
+	cpu_exec(len_of_step);
+	return 0;
 }
 // added by wk: execute by step
 
 
 
 static struct {
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
+	const char *name;
+	const char *description;
+	int (*handler) (char *);
 } cmd_table [] = {
-  { "help", "Display informations about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "Let the program step through N instructions and then pause execution,\nWhen N is not given, it defaults to 1", cmd_si}, //wk
+	{ "help", "Display informations about all supported commands", cmd_help },
+	{ "c", "Continue the execution of the program", cmd_c },
+	{ "q", "Exit NEMU", cmd_q },
+	{ "si", "Let the program step through N instructions and then pause execution,\nWhen N is not given, it defaults to 1", cmd_si}, //wk
 
-  /* TODO: Add more commands */
+	/* TODO: Add more commands */
 
 };
 
 #define NR_CMD ARRLEN(cmd_table)
 
 static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
+	/* extract the first argument */
+	char *arg = strtok(NULL, " ");
+	int i;
 
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-    }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
-    }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
+	if (arg == NULL) {
+		/* no argument given */
+		for (i = 0; i < NR_CMD; i ++) {
+			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+		}
+	}
+	else {
+		for (i = 0; i < NR_CMD; i ++) {
+			if (strcmp(arg, cmd_table[i].name) == 0) {
+				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+				return 0;
+			}
+		}
+		printf("Unknown command '%s'\n", arg);
+	}
+	return 0;
 }
 
 void sdb_set_batch_mode() {
-  is_batch_mode = true;
+	is_batch_mode = true;
 }
 
 void sdb_mainloop() {
-  if (is_batch_mode) {
-    cmd_c(NULL);
-    return;
-  }
+	if (is_batch_mode) {
+		cmd_c(NULL);
+		return;
+	}
 
-  for (char *str; (str = rl_gets()) != NULL; ) {
-    char *str_end = str + strlen(str);
+	for (char *str; (str = rl_gets()) != NULL; ) {
+		char *str_end = str + strlen(str);
 
-    /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
+		/* extract the first token as the command */
+		char *cmd = strtok(str, " ");
+		if (cmd == NULL) { continue; }
 
-    /* treat the remaining string as the arguments,
-     * which may need further parsing
-     */
-    char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end) {
-      args = NULL;
-    }
+		/* treat the remaining string as the arguments,
+		 * which may need further parsing
+		 */
+		char *args = cmd + strlen(cmd) + 1;
+		if (args >= str_end) {
+			args = NULL;
+		}
 
 #ifdef CONFIG_DEVICE
-    extern void sdl_clear_event_queue();
-    sdl_clear_event_queue();
+		extern void sdl_clear_event_queue();
+		sdl_clear_event_queue();
 #endif
 
-    int i;
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
-        break;
-      }
-    }
+		int i;
+		for (i = 0; i < NR_CMD; i ++) {
+			if (strcmp(cmd, cmd_table[i].name) == 0) {
+				if (cmd_table[i].handler(args) < 0) { return; }
+				break;
+			}
+		}
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-  }
+		if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+	}
 }
 
 void init_sdb() {
-  /* Compile the regular expressions. */
-  init_regex();
+	/* Compile the regular expressions. */
+	init_regex();
 
-  /* Initialize the watchpoint pool. */
-  init_wp_pool();
+	/* Initialize the watchpoint pool. */
+	init_wp_pool();
 }
