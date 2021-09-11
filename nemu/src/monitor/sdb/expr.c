cab7c328d04a81e8c4ab92ec4cce99fb57149c49
tracer-ics2021 <tracer@njuics.org>
2021-09-12 07:37:11 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 876967228e534f6aac4c1d0e4e0c1964     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  07:37:11 up 30 min,  1 user,  load average: 0.05, 0.04, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -70,8 +70,8 @@ typedef struct token {
 	int type;
 	char str[32];
 } Token;
-// wk modify len, old: 32
-static Token tokens[30000] __attribute__((used)) = {};
+// wk modify len for test, old: 32
+static Token tokens[65535] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 // wk:  remove a token from tokens  
