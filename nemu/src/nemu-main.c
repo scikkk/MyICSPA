5e7ab651677c5274d63160bc53b7863d2c42b216
tracer-ics2021 <tracer@njuics.org>
2021-09-11 20:57:14 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 8abfe428df45453b8969495671b8c98b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:57:14 up 27 min,  1 user,  load average: 0.15, 0.09, 0.10

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,20 +1,15 @@
 #include <common.h>
+//#include <nemu/src/monitor/sdb/sdb.h>
 
-void init_monitor(int, char *[]);
-void am_init_monitor();
-void engine_start();
-int is_exit_status_bad();
+
+word_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
-#ifdef CONFIG_TARGET_AM
-  am_init_monitor();
-#else
-  init_monitor(argc, argv);
-#endif
-
-  /* Start engine. */
-  engine_start();
-
-  return is_exit_status_bad();
+  bool s;
+  bool *ps = &s;
+	// bool *s = (bool*)malloc(sizeof(bool));
+  uint32_t res =  expr("1+1",ps);
+  printf("%u\n", res);
+  // free(s);
+  return 0;
 }
