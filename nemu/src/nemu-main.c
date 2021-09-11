9b1fe30028448511eb163b5c288a32c29fad658a
tracer-ics2021 <tracer@njuics.org>
2021-09-11 22:31:57 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 8abfe428df45453b8969495671b8c98b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  22:31:57 up  2:02,  1 user,  load average: 0.01, 0.03, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,8 +19,8 @@ int main(int argc, char *argv[]) {
 	char e[65536];
 	//int num = scanf("%s", e);
 	//assert(num == 1);
-	FILE *fpread = fopen("/src/in0put.txt", "r");
-	FILE *fpwrite = fopen("/src/wkres.txt", "w");
+	FILE *fpread = fopen("/src/.input.txt", "r");
+	FILE *fpwrite = fopen("/src/.wkres.txt", "w");
 	assert(fpread != NULL || fpwrite == NULL);
 		printf("1:%s\n", e);
 	while (fgets(e, sizeof(e), fpread) != NULL) {
