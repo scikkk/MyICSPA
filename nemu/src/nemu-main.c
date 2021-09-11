672cd28e81df95d1ed286dcb5a37713d539a923c
tracer-ics2021 <tracer@njuics.org>
2021-09-11 21:01:09 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 8abfe428df45453b8969495671b8c98b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:01:09 up 31 min,  1 user,  load average: 0.16, 0.10, 0.09

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -7,7 +7,7 @@ word_t expr(char *e, bool *success);
 int main(int argc, char *argv[]) {
   bool *s = (bool*)malloc(sizeof(bool));
   *s = true;
-  uint32_t res =  expr("1+1",s);
+  uint32_t res =  expr("1+1\0",s);
   printf("%u\n", res);
   free(s);
   return 0;
