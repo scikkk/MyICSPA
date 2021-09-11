6224d174ccbaee3ca1f958a2c0fcb82be1ae7ffc
tracer-ics2021 <tracer@njuics.org>
2021-09-11 21:31:21 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 8abfe428df45453b8969495671b8c98b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:31:21 up  1:01,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,15 +1,27 @@
-
 #include <common.h>
 //#include <nemu/src/monitor/sdb/sdb.h>
 
 word_t expr(char *e, bool *success);
+void init_monitor(int, char *[]);
+void am_init_monitor();
+void engine_start();
+int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {   
+
+	/* Initialize the monitor. */
+#ifdef CONFIG_TARGET_AM
+	am_init_monitor();
+#else
+	init_monitor(argc, argv);
+#endif  
+
+
 	char e[65536] = "";
 	int num = scanf("%s", e);
 	assert(num == 1);
 	printf("%s\n", e);
-	
+
 	bool success = true; 
 	uint32_t res = expr(e, &success);
 	if (success) {
@@ -18,6 +30,6 @@ int main(int argc, char *argv[]) {
 	else {
 		printf("表达式不合法！\n");
 	}
-	
+
 	return 0;
 }
