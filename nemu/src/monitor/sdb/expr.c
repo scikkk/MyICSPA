54ea9918c8dda3957ea80804b9447dec2090dc47
tracer-ics2021 <tracer@njuics.org>
2021-09-15 17:28:04 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: becf8690b28e461989744838d2487059     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  17:28:04 up  1:41,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -153,11 +153,16 @@ static bool make_token(char *e) {
 					case TK_NOTYPE: break;
 					case TK_REG : strncpy(tokens[nr_token].str,substr_start+1,substr_len);
 								 tokens[nr_token].str[substr_len] = '\0';
+								  tokens[nr_token].type = rules[i].token_type;
+							 nr_token++;
+							 break;
 
 					case TK_INT: strncpy(tokens[nr_token].str,substr_start,substr_len);
 								 tokens[nr_token].str[substr_len] = '\0';
-					default: tokens[nr_token].type = rules[i].token_type;
+								 tokens[nr_token].type = rules[i].token_type;
 							 nr_token++;
+							 break;
+			
 				}
 				break;
 			}
