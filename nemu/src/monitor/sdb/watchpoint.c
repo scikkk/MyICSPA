7a13c9a815fbebeca12a67967130b243eee047ff
tracer-ics2021 <tracer@njuics.org>
2021-09-16 15:23:41 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b4c8c6642b30450c9516b90b8b0ac631     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  15:23:41 up 17 min,  1 user,  load average: 0.22, 0.45, 0.33

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -7,11 +7,13 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
+  char expr[32];
+  struct watchpoint *prev;
 
 } WP;
 
 static WP wp_pool[NR_WP] = {};
-static WP *head = NULL, *free_ = NULL;
+static WP *head = NULL, *tail = NULL, *free_ = NULL;
 
 void init_wp_pool() {
   int i;
@@ -27,3 +29,42 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+
+
+WP* new_wp(const char* expr){
+	if (free_ == NULL){printf("%s\n", "No more free space!\0");assert(0);}
+	WP *new_w = free_;
+	new_w->next = NULL;
+	new_w->prev = tail;
+	free_ = free_->next;
+	if (head == NULL){
+		head = new_w;
+		tail = new_w;
+		head->NO = 1;
+	}
+	else {
+		new_w->NO = tail->NO + 1;
+		tail->next = new_w;
+		tail = tail->next;
+	}
+	strcpy(tail->expr,expr);
+	return tail;
+}
+void free_wp(WP *wp){
+	if (wp->prev==NULL){
+		head = wp->next;
+		head->prev = NULL;
+	} 
+	else if(wp->next==NULL){
+	tail = wp->prev;
+	tail->next=NULL;
+	}
+	else{
+		wp->prev->next = wp->next;
+		wp->next->prev = wp->prev;
+		wp->next=free_;
+		free_->prev=wp;
+		free_=free_->prev;
+	}
+	return ;	
+}
