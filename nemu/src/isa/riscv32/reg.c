759bc3483e1ed8f3df2cb8e0eb6eb73492b3b8c5
tracer-ics2021 <tracer@njuics.org>
2021-09-17 09:32:25 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e350db942e0c4144b3603d0ac675ea4f     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  09:32:25 up 26 min,  1 user,  load average: 0.11, 0.07, 0.16

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -2,29 +2,30 @@
 #include "local-include/reg.h"
 
 const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+	"$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+	"s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+	"a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+	"s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
 void isa_reg_display() {
 	// added by wk
 	for (int k = 0; k <32; k++){
-	printf("%-8s\t0x%-8x\t%d\n", regs[k], gpr(k), gpr(k));
+		printf("%-8s\t0x%-8x\t%d\n", regs[k], gpr(k), gpr(k));
 	}		
 	// added by wk
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
 	char ss[5];
-strcpy(ss,s);
+	strcpy(ss,s);
+	if(strcmp(ss,"pc")==0) return cpu.pc;
 	for (int k = 0; ss[k] != '\0'; k++){
-	if(s[k] >= 'A' && s[k] <= 'Z') ss[k]+=32;
+		if(s[k] >= 'A' && s[k] <= 'Z') ss[k]+=32;
 	}
-  for (int k = 0; k < 32; k++) {
-  if (strcmp(ss,regs[k]) == 0) return gpr(k);
-  }
-  *success = false;
+	for (int k = 0; k < 32; k++) {
+		if (strcmp(ss,regs[k]) == 0) return gpr(k);
+	}
+	*success = false;
 	return -1;
 }
