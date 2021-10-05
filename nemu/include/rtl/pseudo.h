f3848b9230582840aeb771e68899b7cc297b87a5
tracer-ics2021 <tracer@njuics.org>
2021-10-05 23:06:03 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 6c34cd6d066d4ae1a001dad3ebec370d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  23:06:03 up 23 min,  1 user,  load average: 0.05, 0.09, 0.10

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -8,35 +8,45 @@
 /* RTL pseudo instructions */
 
 static inline def_rtl(li, rtlreg_t* dest, const rtlreg_t imm) {
-  rtl_addi(s, dest, rz, imm);
+	rtl_addi(s, dest, rz, imm);
 }
 
 static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
-  rtl_addi(s, dest, src1, 0);
+	rtl_addi(s, dest, src1, 0);
 }
 
 static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
-  // dest <- ~src1
-  TODO();
+	// dest <- ~src1
+	// TODO();
+	rtl_xori(s, dest, src1, 0xffffffff);
+	// wk 2.1
 }
 
 static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
-  // dest <- -src1
-  TODO();
+	// dest <- -src1
+	// TODO();
+	rtl_xori(s, dest, src1, 0x80000000);
+	// wk 2.1
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+	// dest <- signext(src1[(width * 8 - 1) .. 0])
+	// TODO();
+	rtl_slli(s, dest, src1, width);
+	rtl_srai(s, dest, src1, width);
+	// wk 2.1
 }
 
 static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- zeroext(src1[(width * 8 - 1) .. 0])
-  TODO();
-}
+	// dest <- zeroext(src1[(width * 8 - 1) .. 0])
+	// TODO();
+	rtl_slli(s, dest, src1, width);
+	rtl_srli(s, dest, src1, width);
+	// wk 2.1
+	}
 
 static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- src1[width * 8 - 1]
-  TODO();
+	// dest <- src1[width * 8 - 1]
+	TODO();
 }
 #endif
