8f5e0157a20cd7fac2d4e2ec671d92b2b326ab57
tracer-ics2021 <tracer@njuics.org>
2021-10-07 10:37:29 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: cf43de56f0a04508808f9d87084c45fd     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  10:37:29 up  2:09,  1 user,  load average: 0.02, 0.08, 0.03

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -57,9 +57,9 @@ static def_DHelper(J) {
 }
 static def_DHelper(B) {
 	decode_op_r(s, id_src1, s->isa.instr.b.rs1, false);
+	decode_op_r(s, id_src2, s->isa.instr.b.rs2, false);
 	sword_t simm = (s->isa.instr.b.simm12 << 12) | s->isa.instr.b.imm11 << 11 | s->isa.instr.b.imm10_5 << 5 | s->isa.instr.b.imm4_1 << 1;
 	decode_op_i(s, id_dest, simm, false);
-	decode_op_r(s, id_src2, s->isa.instr.b.rs2, false);
 }
 // wk 2.1
 
