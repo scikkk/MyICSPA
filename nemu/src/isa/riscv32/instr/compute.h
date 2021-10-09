7646dee85ac1d23791bde59effb1ab727c36149b
tracer-ics2021 <tracer@njuics.org>
2021-10-09 15:39:46 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b8e6684a1b8a4896abdbcf5cebe05939     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  15:39:46 up  2:31,  1 user,  load average: 0.00, 0.00, 0.01

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -11,13 +11,13 @@ def_EHelper(auipc) {
 
 def_EHelper(jal) {
 	rtl_addi(s,ddest,&cpu.pc,0x4);
-	rtl_addi(s, &(s->dnpc),&cpu.pc, id_src1->simm);
+	rtl_j(s, cpu.pc + id_src1->simm);
 
 }
 
 def_EHelper(jalr) {
 	rtl_addi(s, ddest,&cpu.pc, 0x4);
-	rtl_addi(s,&(s->dnpc),dsrc1,id_src2->simm);
+	rtl_j(s, *dsrc1 + id_src2->simm);
 }
 def_EHelper(addi) {
 	rtl_addi(s,ddest,dsrc1,id_src2->simm);
@@ -83,38 +83,38 @@ def_EHelper(sra) {
 
 def_EHelper(beq) {
 	if (*dsrc1 == *dsrc2)	{
-		rtl_addi(s, &(s->dnpc),&cpu.pc, id_dest->simm);
+		rtl_j(s, cpu.pc + id_dest->simm);
 	}
 }
 
 def_EHelper(bne) {
 	if (*dsrc1 != *dsrc2)	{
 		/* printf("%-10x\n%d\n",cpu.pc,id_dest->simm); */
-
-		s->dnpc = cpu.pc + id_dest->simm;
+        rtl_j(s, cpu.pc + id_dest->simm);
 		/* rtl_addi(s, &(s->dnpc),&cpu.pc, id_dest->simm); */
 	}
 }
 
 def_EHelper(blt) {
 	if (*dsrc1 < *dsrc2)	{
-		rtl_addi(s, &(s->dnpc),&cpu.pc, id_dest->simm);
+		rtl_j(s, cpu.pc + id_dest->simm);
 	}
 }
 
 def_EHelper(bge) {
 	if (*dsrc1 >= *dsrc2)	{
-		rtl_addi(s, &(s->dnpc),&cpu.pc, id_dest->simm);
-	}}
+		rtl_j(s, cpu.pc + id_dest->simm);
+	}
+}
 def_EHelper(bltu) {
 	if ((unsigned)*dsrc1 < (unsigned)*dsrc2)	{
-		rtl_addi(s, &(s->dnpc),&cpu.pc, id_dest->simm);
+		rtl_j(s, cpu.pc + id_dest->simm);
 	}
 }
 
 def_EHelper(bgeu) {
 	if ((unsigned)*dsrc1 >= (unsigned)*dsrc2)	{
-		rtl_addi(s, &(s->dnpc),&cpu.pc, id_dest->simm);
+		rtl_j(s, cpu.pc + id_dest->simm);
 	}
 }
 
