05dc8483543a342f2997fd984d7f6d3a0154b1b2
tracer-ics2021 <tracer@njuics.org>
2021-10-10 20:32:18 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: cc18024a3299494eb264e18ac77d6318     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:32:18 up 58 min,  1 user,  load average: 0.06, 0.04, 0.00

--- nemu/src/isa/riscv32/instr/contorl.h
+++ nemu/src/isa/riscv32/instr/contorl.h
@@ -18,31 +18,31 @@ def_EHelper(beq) {
 
 def_EHelper(bne) {
 	if (interpret_relop(RELOP_NE,*dsrc1,*dsrc2)){
-		rtl_j(s,id_dest->simm+cpu.pc);
+		rtl_j(s,cpu.pc+id_dest->simm);
 	}
 }
 
 def_EHelper(blt) {
 	if (interpret_relop(RELOP_LT,*dsrc1,*dsrc2)){
-		 rtl_j(s,id_dest->simm+cpu.pc);
+		 rtl_j(s,cpu.pc+id_dest->simm);
 	}
 }
 
 def_EHelper(bge) {
 	if (interpret_relop(RELOP_GE,*dsrc1,*dsrc2)){
-		 rtl_j(s,id_dest->simm+cpu.pc);   
+		 rtl_j(s,cpu.pc+id_dest->simm);   
 	}
 }
 
 def_EHelper(bltu) {
 	if (interpret_relop(RELOP_LTU,*dsrc1,*dsrc2)){
-	 rtl_j(s,id_dest->simm+cpu.pc);   
+	 rtl_j(s,cpu.pc+id_dest->simm);   
 	}
 }
 
 def_EHelper(bgeu) {
 	if (interpret_relop(RELOP_GEU,*dsrc1,*dsrc2)){
-	 rtl_j(s,id_dest->simm+cpu.pc);   
+	 rtl_j(s,cpu.pc+id_dest->simm);   
 	}
 }
 
