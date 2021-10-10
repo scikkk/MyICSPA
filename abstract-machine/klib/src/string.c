393984f69a28c4e473172b70caca3f80f0552a17
tracer-ics2021 <tracer@njuics.org>
2021-10-10 22:32:06 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: cc18024a3299494eb264e18ac77d6318     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:32:06 up  2:32,  1 user,  load average: 0.01, 0.06, 0.49

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,13 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  /* panic("Not implemented"); */
+  assert(NULL != s);
+	int len = 0;
+	while(*s++ != '\0'){
+		len++;
+	}
+	return len;
 }
 
 char *strcpy(char *dst, const char *src) {
