a8d43cfcb559086f1b6d23aa3cede51a806706a5
tracer-ics2021 <tracer@njuics.org>
2021-10-11 17:27:43 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 9c9de4854f4a4b5ba0a2a72686250317     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  17:27:43 up  1:12,  1 user,  load average: 0.09, 0.03, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,23 +6,78 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	/* panic("Not implemented"); */
+	int ret = -1;
+	va_list ap;
+	va_start(ap, fmt);
+	while(*fmt != '\0'){
+		if(*fmt != '%'){
+			*out++ = *fmt++;
+			*out = '\0';
+		}
+		else{
+			fmt++;
+			switch(*fmt){
+				case 's':{
+							 char *valstr = va_arg(ap, char*);
+							 strcat(out, valstr);
+							 out += strlen(valstr);
+							 ret += strlen(valstr);
+							 fmt++;
+							 break;
+						 }
+				case 'd':{
+							 int valint = va_arg(ap, int);
+							 if(valint < 0){
+								*out++ = '-';
+								valint = -valint;
+							 }
+							 char *p = out;
+							 char *q = out;
+							 do{
+								ret++;
+								*q++ = (char)(valint%10 + '0');
+								valint /= 10;
+							 }while(valint>0);
+							 *q = '\0';
+							 out = q;
+							 q--;
+							 while(q > p){
+								char temp = *p;
+								*p++ = *q;
+								*q-- = temp;
+							 }
+
+							 fmt++;
+
+						 }
+				default: {
+							 *out++ = *fmt++;
+							 *out = '\0';
+						 }
+			}
+			assert(*out == '\0');
+		}
+	}
+	va_end(ap);
+	return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 #endif
