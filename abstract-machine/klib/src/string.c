647d1b945079097d0def8b28a213cc28ee74ad18
tracer-ics2021 <tracer@njuics.org>
2021-10-11 09:05:36 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e50308c52437471d9a3c63a1e21462e6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  09:05:36 up  1:22,  1 user,  load average: 0.09, 0.07, 0.07

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -149,19 +149,19 @@ void *memcpy(void *out, const void *in, size_t n) {
 int memcmp(const void *s1, const void *s2, size_t n) {
 	/* panic("Not implemented"); */
 	assert(s1 != NULL && s2 != NULL);
-	/* if(n == 0) return 0; */
-	/* while(n > 0 && (*(char*)s1 == *(char*)s2)){ */
-	/* 	(char*)s1++; */
-	/* 	(char*)s2++; */
-	/* 	n--; */
-	/* } */
-	/* // printf("strcmp: s1: %s; s2: %s; out: %d\n", *(char*)s1, *(char*)s2, (*((unsigned char *)s1) - *((unsigned char *)s2))); */
-	/* if (*(char*)s1 > *(char*)s2){ */
-	/* 	return 1; */
-	/* } */
-	/* else if(*(char*)s1 < *(char*)s2){ */
-	/* 	return -1; */
-	/* } */
+	if(n == 0) return 0;
+	while(n > 0 && (*(char*)s1 == *(char*)s2)){
+		(char*)s1++;
+		(char*)s2++;
+		n--;
+	}
+	// printf("strcmp: s1: %s; s2: %s; out: %d\n", *(char*)s1, *(char*)s2, (*((unsigned char *)s1) - *((unsigned char *)s2)));
+	if (*(char*)s1 > *(char*)s2){
+		return 1;
+	}
+	else if(*(char*)s1 < *(char*)s2){
+		return -1;
+	}
 	return 0;
 }
 
