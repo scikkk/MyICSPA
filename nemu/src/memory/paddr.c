ed8e5a3a655e3e1cd67876069ca45d9462b60c4d
tracer-ics2021 <tracer@njuics.org>
2021-10-13 22:01:06 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 59d8f7a0887341d4a8dd504f028c4026     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:01:06 up  3:31,  1 user,  load average: 0.01, 0.05, 0.01

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -13,42 +13,45 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host(addr), len);
-  return ret;
+	word_t ret = host_read(guest_to_host(addr), len);
+	return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
-  host_write(guest_to_host(addr), len, data);
+	host_write(guest_to_host(addr), len, data);
 }
 
 void init_mem() {
 #if   defined(CONFIG_TARGET_AM)
-  pmem = malloc(CONFIG_MSIZE);
-  assert(pmem);
+	pmem = malloc(CONFIG_MSIZE);
+	assert(pmem);
 #endif
 #ifdef CONFIG_MEM_RANDOM
-  uint32_t *p = (uint32_t *)pmem;
-  int i;
-  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
-    p[i] = rand();
-  }
+	uint32_t *p = (uint32_t *)pmem;
+	int i;
+	for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
+		p[i] = rand();
+	}
 #endif
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]",
-      (paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
+	Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]",
+			(paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  static int count = 0;
-  printf("[read] count:%-4d begin:0x%-9x end:0x%-9x res:0x%-9x\n", count++, addr, addr+len, pmem_read(addr, len));
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
-    panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
-      addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
+	static int count = 0;
+	if (likely(in_pmem(addr))){
+		word_t ret = pmem_read(addr, len);
+		printf("[read] count:%-4d begin:0x%-9x end:0x%-9x res:0x%-9x %d\n", count++, addr, addr+len, ret, ret);
+		return ret;
+	}
+	MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
+			panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
+				addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
-    panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
-      addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
+	if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+	MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
+			panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
+				addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
 }
