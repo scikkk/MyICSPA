fc0e04808ab248f5f445149b811ec6b255c45b96
tracer-ics2021 <tracer@njuics.org>
2021-10-13 17:58:28 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 59d8f7a0887341d4a8dd504f028c4026     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  17:58:28 up  1:38,  1 user,  load average: 0.03, 0.03, 0.00

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -4,10 +4,10 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-  /* 0x800002b7,  // lui t0,0x80000 */
-  /* 0x0002a023,  // sw  zero,0(t0) */
-  /* 0x0002a503,  // lw  a0,0(t0) */
-	0x000fa503,
+  0x800002b7,  // lui t0,0x80000
+  0x0002a023,  // sw  zero,0(t0)
+  0x0002a503,  // lw  a0,0(t0)
+  0x000fa503,  // wrong instr, wk add for 2.2
   0x0000006b,  // nemu_trap
 };
 
