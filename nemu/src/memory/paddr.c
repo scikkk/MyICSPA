9ab0e93f6f2fa84983848418f05a3c570d3e0dc3
tracer-ics2021 <tracer@njuics.org>
2021-10-13 22:05:02 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 59d8f7a0887341d4a8dd504f028c4026     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:05:02 up  3:35,  1 user,  load average: 0.13, 0.11, 0.04

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -41,7 +41,7 @@ word_t paddr_read(paddr_t addr, int len) {
 	static int count = 0;
 	if (likely(in_pmem(addr))){
 		word_t ret = pmem_read(addr, len);
-		printf(" [read] count:%-6d begin:0x%-9x end:0x%-9x data:0x%-9x %d\n", count++, addr, addr+len, ret, ret);
+		printf("[read ] count:%-6d begin:0x%-9x end:0x%-9x data:0x%-9x %d\n", count++, addr, addr+len, ret, ret);
 		return ret;
 	}
 	MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
