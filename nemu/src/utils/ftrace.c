13b053f10eba62c2990532c7784b46215dfe590a
tracer-ics2021 <tracer@njuics.org>
2021-10-18 18:02:35 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b90312613c0f41dea15ad501f74746de     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:02:35 up  1:44,  1 user,  load average: 0.02, 0.07, 0.06

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -5,13 +5,13 @@ FILE *elf_fp = NULL;
 
 void init_ftrace(const char *elf_file) {
   elf_fp = stdout;
-  printf("elf_file: %s\n", elf_file);
+  /* printf("elf_file: %s\n", elf_file); */
   if (elf_file != NULL) {
     FILE *fp = fopen(elf_file, "r");
     Assert(fp, "Can not open '%s'", elf_file);
     elf_fp = fp;
   }
-  Log("Symbol table is loaded from %s", elf_file ? elf_file : "stdout");
+  Log("Symbol table is loaded from %s", elf_file ? elf_file : "NULL(no .elf file has been read)");
 }
 
 /* bool ftrace_enable() { */
