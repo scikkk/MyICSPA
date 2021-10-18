334203a81cd2a0f34a5e8aa919487ffa046946e6
tracer-ics2021 <tracer@njuics.org>
2021-10-18 09:53:55 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: dc8e8eaec3b44b018d6a4662aa4f645b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  09:53:57 up 47 min,  1 user,  load average: 0.39, 0.13, 0.09

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -37,12 +37,20 @@ void init_mem() {
 			(paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
 }
 
+bool mtrace_enable(paddr_t addr){
+#ifdef CONFIG_MTRACE_COND 
+  return MTRACE_COND;
+#endif
+  return true;
+}
+
 word_t paddr_read(paddr_t addr, int len) {
 	if (likely(in_pmem(addr))){
 
 		word_t ret = pmem_read(addr, len);
 #ifdef CONFIG_MTRACE
 		static int count = 1;
+		if (mtrace_enable(addr))
 		printf("[read ] count:%-6d begin:0x%-10x end:0x%-10x data:0x%-9x %-16d\n", count++, addr, addr+len, ret, ret);
 #endif
 		return ret;
@@ -57,6 +65,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACE
 		static int count = 1;
 		word_t old = pmem_read(addr, len);
+		if (mtrace_enable(addr))
 		printf("[write] count:%-6d begin:0x%-10x end:0x%-10x old :0x%-9x %-16d new :0x%-9x %d\n", count++, addr, addr+len,old,old, data, data);
 #endif 
 		pmem_write(addr, len, data); 
