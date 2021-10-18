f6d2a10c8809c6ca275f7f7c81528cd52f7d286b
tracer-ics2021 <tracer@njuics.org>
2021-10-18 18:31:33 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b90312613c0f41dea15ad501f74746de     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:31:34 up  2:13,  1 user,  load average: 0.18, 0.12, 0.03

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -1,17 +1,24 @@
 #include <common.h>
+#include <elf.h>
+
 
 /* extern uint64_t g_nr_guest_instr; */
 FILE *elf_fp = NULL;
 
 void init_ftrace(const char *elf_file) {
-  elf_fp = stdout;
-  /* printf("elf_file: %s\n", elf_file); */
-  if (elf_file != NULL) {
-    FILE *fp = fopen(elf_file, "r");
-    Assert(fp, "Can not open '%s'", elf_file);
-    elf_fp = fp;
-  }
-  Log("Symbol table is loaded from %s", elf_file ? elf_file : "NULL(no .elf file has been read)");
+	elf_fp = stdout;
+	Elf32_Ehdr elfheader;
+
+	/* printf("elf_file: %s\n", elf_file); */
+
+	FILE *fp = fopen(elf_file, "r");
+	Assert(fp, "Can not open '%s'", elf_file);
+    
+	elf_fp = fp;
+
+	assert(fread(&elfheader,32,1,elf_fp)==1);
+	printf("%hx\n",elfheader.e_shstrndx);
+	Log("Symbol table is loaded from %s", elf_file);
 }
 
 /* bool ftrace_enable() { */
