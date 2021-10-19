b29c93adaea806cae1ab831f14ebd4f5ecf941d3
tracer-ics2021 <tracer@njuics.org>
2021-10-19 22:58:58 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 6eec1be1588046118b8773a8d86e4a58     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:58:58 up  1:51,  1 user,  load average: 0.03, 0.06, 0.01

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -25,7 +25,8 @@ void tableheader(const char *pbuff)
 			for(int idx = 0; idx < ncount; idx++)
 			{
 				printf("%d:\t", idx);
-				printf("%u %u %u %u %u %u\n",psym->st_value, psym->st_size,psym->st_info,psym->st_other,psym->st_shndx,psym->st_name);
+				
+				printf("%u %u %u %u %u %u\n",psym->st_value, psym->st_size,ELF32_ST_TYPE(psym->st_info),psym->st_other,psym->st_shndx,psym->st_name);
 			}
 			/* printf("pbuffstr: %s ncount: %d\n", pbuffstr, ncount); */ 
 			continue;
