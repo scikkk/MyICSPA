317f2f16fa69153be2c29f29cc271ad70bfee655
tracer-ics2021 <tracer@njuics.org>
2021-10-19 20:31:09 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 6eec1be1588046118b8773a8d86e4a58     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:31:09 up 11 min,  1 user,  load average: 0.14, 0.28, 0.34

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -2,22 +2,52 @@
 #include <elf.h>
 
 
-/* extern uint64_t g_nr_guest_instr; */
+/* extern uint32_t g_nr_guest_instr; */
 FILE *elf_fp = NULL;
+
+void tableheader(const char *pbuff)
+{
+	//从节区里面定位到偏移
+	Elf32_Ehdr* pfilehead = (Elf32_Ehdr*)pbuff;
+	Elf32_Half eshstrndx = pfilehead->e_shstrndx;
+	Elf32_Shdr* psecheader = (Elf32_Shdr*)(pbuff + pfilehead->e_shoff);
+	Elf32_Shdr* pshstr = (Elf32_Shdr*)(psecheader + eshstrndx);
+	char* pshstrbuff = (char *)(pbuff + pshstr->sh_offset);
+
+	for(int i = 0;i<pfilehead->e_shnum;++i)
+	{
+		if(!strcmp(psecheader[i].sh_name + pshstrbuff, ".dynsym") || !strcmp(psecheader[i].sh_name + pshstrbuff, ".symtab"))
+		{
+			//    Elf32_Sym* psym = (Elf32_Sym*)(pbuff + psecheader[i].sh_offset);
+			int ncount = psecheader[i].sh_size / psecheader[i].sh_entsize;
+			char* pbuffstr = (char*)((psecheader + psecheader[i].sh_link)->sh_offset + pbuff);
+			printf("Symbol table '%s' contains %d entries:\r\n", psecheader[i].sh_name + pshstrbuff, ncount);
+			/* printf("%s %s %d",*psym, *pbuffstr, ncount); */
+
+			printf(" %s %d", pbuffstr, ncount); 
+			continue;
+		}
+	}
+}
+
 
 void init_ftrace(const char *elf_file) {
 	elf_fp = stdout;
-	Elf32_Ehdr elfheader;
 
 	/* printf("elf_file: %s\n", elf_file); */
 
 	FILE *fp = fopen(elf_file, "r");
 	Assert(fp, "Can not open '%s'", elf_file);
-    
-	elf_fp = fp;
 
-	assert(fread(&elfheader,32,1,elf_fp)==1);
-	printf("%hx\n",elfheader.e_shstrndx);
+	elf_fp = fp;
+	int file_size = ftell(fp);
+	char *elf_str = (char*)malloc(sizeof(char)*file_size);
+	fseek(elf_fp, 0, SEEK_SET);
+
+	unsigned long ret =	fread(elf_str, sizeof(char), file_size, elf_fp);
+	assert(ret == file_size);
+	fclose(elf_fp);
+	tableheader(elf_str);
 	Log("Symbol table is loaded from %s", elf_file);
 }
 
