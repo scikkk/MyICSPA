bd1409cd5d9e823332f8984222e1025f500056b7
tracer-ics2021 <tracer@njuics.org>
2021-10-19 23:09:32 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 6eec1be1588046118b8773a8d86e4a58     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  23:09:32 up  2:02,  1 user,  load average: 0.09, 0.04, 0.01

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -20,13 +20,13 @@ void tableheader(const char *pbuff)
 		{
 			Elf32_Sym* psym = (Elf32_Sym*)(pbuff + psecheader[i].sh_offset);
 			int ncount = psecheader[i].sh_size / psecheader[i].sh_entsize;
-			/* char* pbuffstr = (char*)((psecheader + psecheader[i].sh_link)->sh_offset + pbuff); */
+			char* pbuffstr = (char*)((psecheader + psecheader[i].sh_link)->sh_offset + pbuff);
 			printf("Symbol table '%s' contains %d entries:\r\n", psecheader[i].sh_name + pshstrbuff, ncount);
-			for(int idx = 0; idx <=ncount; idx++)
+			for(int idx = 0; idx < ncount; idx++)
 			{
 				printf("%d:\t", idx);
 				
-				printf("%u %u %d %d %u %u %u\n",psym->st_value, psym->st_size,ELF32_ST_TYPE(psym->st_info),ELF32_ST_BIND(psym->st_info),psym->st_other,psym->st_shndx,psym->st_name);
+				printf("%u %u %d %d %u %u %s\n",psym->st_value, psym->st_size,ELF32_ST_TYPE(psym->st_info),ELF32_ST_BIND(psym->st_info),psym->st_other,psym->st_shndx,(psym->st_name+pbuffstr));
 			psym++;
 			}
 			/* printf("pbuffstr: %s ncount: %d\n", pbuffstr, ncount); */ 
