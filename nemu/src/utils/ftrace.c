f16d76473e039e071f87ff9c3d7d5d12a9a159ff
tracer-ics2021 <tracer@njuics.org>
2021-10-19 21:04:47 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 6eec1be1588046118b8773a8d86e4a58     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:04:47 up 45 min,  1 user,  load average: 0.14, 0.13, 0.13

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -22,7 +22,7 @@ void tableheader(const char *pbuff)
 			int ncount = psecheader[i].sh_size / psecheader[i].sh_entsize;
 			char* pbuffstr = (char*)((psecheader + psecheader[i].sh_link)->sh_offset + pbuff);
 			printf("Symbol table '%s' contains %d entries:\r\n", psecheader[i].sh_name + pshstrbuff, ncount);
-			printf("psym->st_info: %u pbuffstr: %s ncount: %d\n",psym->st_info, pbuffstr, ncount); 
+			printf("psym->st_name: %u pbuffstr: %s ncount: %d\n",psym->st_name, pbuffstr, ncount); 
 
 			printf("pbuffstr: %s ncount: %d\n", pbuffstr, ncount); 
 			continue;
