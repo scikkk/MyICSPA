0f9695182c9e4c880e8bc7326d2be95dca660698
tracer-ics2021 <tracer@njuics.org>
2021-10-20 16:28:43 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 26451515a03046ff8f51f4200086fe8d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  16:28:43 up 5 min,  1 user,  load average: 0.29, 0.96, 0.57

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -100,6 +100,8 @@ void ftrace_write(paddr_t src, paddr_t dst){
 			cur->is_call = true;
 			cur->pc = src;
 			strcpy(ftrace_res[ftrace_idx-1].name, func_table[k].name);
+
+					printf("wk\t%s\n", ftrace_res[ftrace_idx-1].name);
 			cur->dst = dst;
 			return;
 		}
@@ -125,7 +127,7 @@ void ftrace_display(){
 	for(unsigned k = 0; k < ftrace_idx; k++){
 		cur = &ftrace_res[k];
 		
-					printf("wk\t%s\n", ftrace_res[k].name);
+					/* printf("wk\t%s\n", ftrace_res[k].name); */
 		printf("0x%8x: ",cur->pc);
 		if(cur->is_call){
 		   tab_in(depth++);
