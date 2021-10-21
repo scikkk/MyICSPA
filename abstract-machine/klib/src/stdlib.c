88de8a103870f3699e6dd80c09da89ff8f2a0c54
tracer-ics2021 <tracer@njuics.org>
2021-10-21 20:13:35 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 1e393e0dbd464c10a45821ea58ce45eb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:13:35 up  2:41,  1 user,  load average: 0.10, 0.24, 0.27

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,12 +29,19 @@ int atoi(const char* nptr) {
 	return x;
 }
 
+
+extern char _heap_start;
+
 void *malloc(size_t size) {
 	// On native, malloc() will be called during initializaion of C runtime.
 	// Therefore do not call panic() here, else it will yield a dead recursion:
 	//   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-	panic("Not implemented");
+	/* panic("Not implemented"); */
+	static void *addr = &_heap_start;
+	void *ret = addr;
+	addr += size;
+	return ret;
 #endif
 	return NULL;
 }
