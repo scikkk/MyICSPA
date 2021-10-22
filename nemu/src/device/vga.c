83c66a30f8eb0a8fdeb3e95e48b4bd4b065f7501
tracer-ics2021 <tracer@njuics.org>
2021-10-22 21:30:59 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 318211026dec43a18a687a1c78993654     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:30:59 up  3:30,  1 user,  load average: 0.70, 0.81, 0.55

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -63,7 +63,7 @@ static inline void update_screen() {
 void vga_update_screen() {
 	// TODO: call `update_screen()` when the sync register is non-zero,
 	// then zero out the sync register
-	if (mmio_read(CONFIG_VGA_CTL_MMIO+4,1)){
+	if (mmio_read(CONFIG_VGA_CTL_MMIO+4,2)&0x8000){
 		update_screen();
 		memset(vmem, 0, screen_size());
 	}
