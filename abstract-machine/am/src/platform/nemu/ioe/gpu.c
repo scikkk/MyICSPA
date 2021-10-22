f452722dfa33a4403c9477848e88bffc0d2a54f9
tracer-ics2021 <tracer@njuics.org>
2021-10-22 22:11:13 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 318211026dec43a18a687a1c78993654     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:11:13 up  4:10,  1 user,  load average: 0.24, 0.22, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,10 +14,10 @@
 
 void __am_gpu_init() {
 	int i;
-	
+
 	int w = W;  // TODO: get the correct width
 	int h = H;  // TODO: get the correct height
-    /* w = inl(VGACTL_ADDR+8); */
+	/* w = inl(VGACTL_ADDR+8); */
 	/* h = inl(VGACTL_ADDR+12); */
 
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
@@ -35,14 +35,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-    uint32_t *pixels = ctl->pixels;
+	uint32_t *pixels = ctl->pixels;
 	if (ctl->sync) {
 		outl(SYNC_ADDR, 1);
-		for(int r = y; r < y + h; r++, pixels+=w){
-			for(int c = x; c < x + w; c++){
-				uint32_t p = pixels[c-x];
-				outl(FB_ADDR+c+r*W, p);
-			}
+	}
+	for(int r = y; r < y + h; r++, pixels+=w){
+		for(int c = x; c < x + w; c++){
+			uint32_t p = pixels[c-x];
+			outl(FB_ADDR+c+r*W, p);
 		}
 	}
 	/* ctl->sync = inl(SYNC_ADDR); */
@@ -53,9 +53,9 @@ void __am_gpu_status(AM_GPU_STATUS_T *status) {
 }
 
 /* void __am_gpu_memcpy(AM_GPU_MEMCPY_T *cpy){ */
-	
+
 /* } */
 
 /* void __am_gpu_render(AM_GPU_RENDER_T *render){ */
-	
+
 /* } */
