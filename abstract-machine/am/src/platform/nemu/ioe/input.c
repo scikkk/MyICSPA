45c3745a09fac64fbe7ef2db3c61779c6b04f1d1
tracer-ics2021 <tracer@njuics.org>
2021-10-22 21:55:27 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 318211026dec43a18a687a1c78993654     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:55:28 up  3:54,  1 user,  load average: 0.90, 0.36, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -13,5 +13,4 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	uint32_t key = inl(KBD_ADDR);
 	kbd->keydown = (key)&KEYDOWN_MASK; 
 	kbd->keycode = (key)&(~KEYDOWN_MASK);
-	kbd->keycode = (key);
 }
