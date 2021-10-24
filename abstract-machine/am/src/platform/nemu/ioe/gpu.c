caa66aec9a9e7c5de18dea0c4ecb2a9a7366fb42
tracer-ics2021 <tracer@njuics.org>
2021-10-24 21:57:52 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e2b99a23d011495aac495818e298403d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:57:52 up  1:06,  1 user,  load average: 0.44, 0.35, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,7 +24,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 	*cfg = (AM_GPU_CONFIG_T) {
 		.present = true, .has_accel = false,
-			.width = inb(VGACTL_ADDR), .height = inb(VGACTL_ADDR+2),
+			.width = inw(VGACTL_ADDR), .height = inw(VGACTL_ADDR+2),
 			.vmemsz = W*H*32
 	};
 }
