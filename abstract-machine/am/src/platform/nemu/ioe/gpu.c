644b46ab42f44513d6bf71401eded94bc5bf9385
tracer-ics2021 <tracer@njuics.org>
2021-10-24 22:24:52 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e2b99a23d011495aac495818e298403d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:24:53 up  1:33,  1 user,  load average: 0.34, 0.49, 0.46

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,13 +7,12 @@
 #define H   inw(VGACTL_ADDR) 
 
 void __am_gpu_init() {
-	int i;
-	int w = W;  // TODO: get the correct width
-	int h = H;  // TODO: get the correct height
-	
-	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	for (i = 0; i < w * h; i ++) fb[i] = i;
-	outl(SYNC_ADDR, 1);
+	/* int i; */
+	/* int w = W;  // TODO: get the correct width */
+	/* int h = H;  // TODO: get the correct height */
+	/* uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; */
+	/* for (i = 0; i < w * h; i ++) fb[i] = i; */
+	/* outl(SYNC_ADDR, 1); */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -34,7 +33,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 		}
 	}
 	if (ctl->sync) {
-		outl(SYNC_ADDR, 1);
+		outl(SYNC_ADDR, ctl->sync);
 	}
 	/* outb(VGACTL_ADDR, x); */
 	/* outb(VGACTL_ADDR+1, y); */
