5dfb8555fd73354768d7ab871a8f58b4e2c418bb
tracer-ics2021 <tracer@njuics.org>
2021-10-27 18:22:08 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 121f05f61f8d473396ab6cf58a4785ec     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  18:22:09 up  1:11,  1 user,  load average: 0.21, 0.18, 0.26

--- nemu/src/device/io/port-io.c
+++ nemu/src/device/io/port-io.c
@@ -8,48 +8,47 @@ static int nr_map = 0;
 
 /* device interface */
 void add_pio_map(const char *name, ioaddr_t addr, void *space, uint32_t len, io_callback_t callback) {
-  assert(nr_map < NR_MAP);
-  assert(addr + len <= PORT_IO_SPACE_MAX);
-  maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1,
-    .space = space, .callback = callback };
-  Log("Add port-io map '%s' at [" FMT_PADDR ", " FMT_PADDR "]",
-      maps[nr_map].name, maps[nr_map].low, maps[nr_map].high);
+	assert(nr_map < NR_MAP);
+	assert(addr + len <= PORT_IO_SPACE_MAX);
+	maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1,
+		.space = space, .callback = callback };
+	Log("Add port-io map '%s' at [" FMT_PADDR ", " FMT_PADDR "]",
+			maps[nr_map].name, maps[nr_map].low, maps[nr_map].high);
 
-  nr_map ++;
+	nr_map ++;
 }
 
-// wk 2.3 dtrace
-bool dtrace_enable(IOMap *map){
-#ifdef  CONFIG_DTRACE_COND
-	return DTRACE_COND;
-#endif
-	return true;
-}
-// wk 2.3 dtrace
+/* // wk 2.3 dtrace */
+/* bool dtrace_enable(IOMap *map){ */
+/* #ifdef  CONFIG_DTRACE_COND */
+/* 	return DTRACE_COND; */
+/* #endif */
+/* 	return true; */
+/* } */
+/* // wk 2.3 dtrace */
 
 /* CPU interface */
 uint32_t pio_read(ioaddr_t addr, int len) {
-  assert(addr + len - 1 < PORT_IO_SPACE_MAX);
-  int mapid = find_mapid_by_addr(maps, nr_map, addr);
-  assert(mapid != -1);
-  word_t ret = map_read(addr, len, &maps[mapid]);
-  printf("nsjncsdmkmm c\n");
-  #ifdef CONFIG_DTRACE
-		static int count = 1;
-		if (dtrace_enable(&maps[mapid]))
-		printf("[read ] count:%-6d name:%s data:0x%-9x %-16d\n", count++, maps[mapid].name, ret, ret);
-#endif
-  return ret;
+	assert(addr + len - 1 < PORT_IO_SPACE_MAX);
+	int mapid = find_mapid_by_addr(maps, nr_map, addr);
+	assert(mapid != -1);
+	word_t ret = map_read(addr, len, &maps[mapid]);
+	/* #ifdef CONFIG_DTRACE */
+	/* static int count = 1; */
+	/* if (dtrace_enable(&maps[mapid])) */
+	/* printf("[read ] count:%-6d name:%s data:0x%-9x %-16d\n", count++, maps[mapid].name, ret, ret); */
+	/* #endif */
+	return ret;
 }
 
 void pio_write(ioaddr_t addr, int len, uint32_t data) {
-  assert(addr + len - 1 < PORT_IO_SPACE_MAX);
-  int mapid = find_mapid_by_addr(maps, nr_map, addr);
-  assert(mapid != -1);
-  #ifdef CONFIG_DTRACE
-		static int count = 1;
-		if (dtrace_enable(&maps[mapid]))
-		printf("[write] count:%-6d name:%s data:0x%-9x %-16d\n", count++, maps[mapid].name, data, data);
-#endif
-  map_write(addr, len, data, &maps[mapid]);
+	assert(addr + len - 1 < PORT_IO_SPACE_MAX);
+	int mapid = find_mapid_by_addr(maps, nr_map, addr);
+	assert(mapid != -1);
+	/* #ifdef CONFIG_DTRACE */
+	/* static int count = 1; */
+	/* if (dtrace_enable(&maps[mapid])) */
+	/* printf("[write] count:%-6d name:%s data:0x%-9x %-16d\n", count++, maps[mapid].name, data, data); */
+	/* #endif */
+	map_write(addr, len, data, &maps[mapid]);
 }
