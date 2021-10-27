0e381e6cc8c1be842d5c2bed3cfb6ca60ac442f5
tracer-ics2021 <tracer@njuics.org>
2021-10-27 22:23:47 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 121f05f61f8d473396ab6cf58a4785ec     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  22:23:48 up  4:27,  1 user,  load average: 0.24, 0.36, 1.69

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -8,6 +8,10 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 	assert(offset == 0 || offset == 4);
 	uint64_t us = get_time();
 	if (!is_write && offset == 4) {
+		/* rtc_port_base[0] = (uint32_t)us; */
+		rtc_port_base[1] = us >> 32;
+	}
+	else if(!is_write && offset == 0){
 		rtc_port_base[0] = (uint32_t)us;
 		rtc_port_base[1] = us >> 32;
 	}
