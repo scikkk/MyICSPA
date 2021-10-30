1d48906d4e28470e21db72fe180700f3d46222b2
tracer-ics2021 <tracer@njuics.org>
2021-10-30 23:26:25 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: efdadd8ceacd4714a9ea6d49365076a2     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  23:26:25 up  4:52,  1 user,  load average: 0.16, 0.11, 0.06

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,42 +5,49 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  if (user_handler) {
-    Event ev = {0};
-    switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
-    }
-	/* printf("x0:%d\n",c->gpr[0]); */
-	/* printf("mstatus:%d\nmepc:%d\nmcause:%d\n", c->mstatus,c->mepc,c->mcause); */
-    c = user_handler(ev, c);
-    assert(c != NULL);
-  }
+	if (user_handler) {
+		Event ev = {0};
+		switch (c->mcause) {
+			case EVENT_NULL: ev.event = EVENT_NULL;break;
+			case EVENT_YIELD: ev.event = EVENT_YIELD; break;
+			case EVENT_SYSCALL:ev.event = EVENT_SYSCALL; break;
+			case EVENT_PAGEFAULT:ev.event = EVENT_PAGEFAULT;break;
+			case EVENT_ERROR:ev.event=EVENT_ERROR;break;
+			case EVENT_IRQ_TIMER :ev.event=EVENT_IRQ_TIMER;break;
+			case EVENT_IRQ_IODEV:ev.event=EVENT_IRQ_IODEV;break;
+			default: assert(0);
+		}
+		/* printf("x0:%d\n",c->gpr[0]); */
+		/* printf("mstatus:%d\nmepc:%d\nmcause:%d\n", c->mstatus,c->mepc,c->mcause); */
+		c = user_handler(ev, c);
+		assert(c != NULL);
+	}
 
-  return c;
+	return c;
 }
 
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
-  // initialize exception entry
-  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+	// initialize exception entry
+	asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
-  // register event handler
-  user_handler = handler;
+	// register event handler
+	user_handler = handler;
 
-  return true;
+	return true;
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+	return NULL;
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
+	asm volatile("li a7, -1; ecall");
 }
 
 bool ienabled() {
-  return false;
+	return false;
 }
 
 void iset(bool enable) {
