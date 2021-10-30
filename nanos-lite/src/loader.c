4e2998fb9cf7a097bfb306d5812823eb4c2ffd9e
tracer-ics2021 <tracer@njuics.org>
2021-10-31 00:18:15 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: efdadd8ceacd4714a9ea6d49365076a2     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  00:18:15 up  5:43,  1 user,  load average: 0.18, 0.10, 0.08

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,13 +10,14 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+	TODO();
+	/* size_t size = get_ramdisk_size(); */
+	return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+	uintptr_t entry = loader(pcb, filename);
+	Log("Jump to entry = %p", entry);
+	((void(*)())entry) ();
 }
 
