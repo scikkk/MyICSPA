0b149a35bd18661d86890fb515cd52e2219e9578
tracer-ics2021 <tracer@njuics.org>
2021-11-03 20:53:26 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 34d2f9c368444d4f9926421d3ea1143c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  20:53:26 up 39 min,  1 user,  load average: 0.18, 0.09, 0.08

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -5,16 +5,14 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	bool ret = true;	
 	for(int k = 0; k < 32; k++){
-			/* ret = ret && difftest_check_reg(reg_name(k,0), pc, ref_r->gpr[k]._32, gpr(k)); */
+			ret = ret && difftest_check_reg(reg_name(k,0), pc, ref_r->gpr[k]._32, gpr(k));
 	}
 	/* difftest_check_reg("mtvec  ", pc, ref_r->mtvec, cpu.mtvec); */
     /* ret = ret && difftest_check_reg("mepc   ", pc, ref_r->mepc, cpu.mepc); */
 	/* ret = ret && difftest_check_reg("mstatus", pc, ref_r->mstatus, cpu.mstatus); */
 	/* ret = ret && difftest_check_reg("mcause ", pc, ref_r->mcause, cpu.mcause); */
-	/* ret = ret && difftest_check_reg("pc", pc, ref_r->pc, cpu.pc); */
-static int wkk=0;
-wkk++;
-if(wkk%1000==1)	printf("mepc:%d\tmstatus:%d\tmcause:%d\t\n",ref_r->mepc,ref_r->mstatus,ref_r->mcause);
+	ret = ret && difftest_check_reg("pc", pc, ref_r->pc, cpu.pc);
+
 	/* isa_reg_display(); */
 	return ret;
 
