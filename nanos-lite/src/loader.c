37c7b040382dce0b802558d5390ec279059b22ec
tracer-ics2021 <tracer@njuics.org>
2021-11-03 20:52:12 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 34d2f9c368444d4f9926421d3ea1143c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  20:52:13 up 38 min,  1 user,  load average: 0.16, 0.07, 0.07

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,24 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+// wk 3.2
+// 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+// 把`buf`中的`len`字节写入到ramdisk中`offset`偏移处
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+// 返回ramdisk的大小, 单位为字节
+size_t get_ramdisk_size();
+// wk 3.2
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-	TODO();
+	/* TODO(); */
 	/* size_t size = get_ramdisk_size(); */
+	Elf_Ehdr header;
+	ramdisk_read(&header,0,52);
+	printf("%d\n", header.e_phnum);
 	return 0;
 }
 
