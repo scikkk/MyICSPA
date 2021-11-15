7cb65c3dcb25e0b6ef80a2d123a7817552a8da2e
tracer-ics2021 <tracer@njuics.org>
2021-11-15 17:53:33 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 8bdd46ee5bf643e58aadd6eff1875ed3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  17:53:33 up  1:14,  1 user,  load average: 0.27, 0.18, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -50,17 +50,17 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 		ramdisk_read(&ph, off, header.e_phentsize);
 		off += header.e_phentsize;
 		if(ph.p_type == PT_LOAD){
-			char seg[65536];
-			ramdisk_read(seg, ph.p_offset, ph.p_filesz);
+			/* char seg[65536]; */
+			ramdisk_read((void*)ph.p_vaddr, ph.p_offset, ph.p_filesz);
 			
 
-			printf("\n");
-			for(int k = 0; k < 1000; k++){printf("%c", seg[k]);}
-			printf("\n");
-			printf("\n");
+			/* printf("\n"); */
+			/* for(int k = 0; k < 1000; k++){printf("%c", seg[k]);} */
+			/* printf("\n"); */
+			/* printf("\n"); */
 
 
-			memcpy((void*)ph.p_vaddr, seg, ph.p_filesz);
+			/* memcpy((void*)ph.p_vaddr, seg, ph.p_filesz); */
 			/* Log("file:%x\tmem:%x\toff:%x\n", ph.p_filesz,ph.p_memsz,ph.p_offset); */
 			if(ph.p_memsz > ph.p_filesz){
 
