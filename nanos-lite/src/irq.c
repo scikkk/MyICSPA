689c1ef1623826c458f694f84eea2b2c7afa6eff
tracer-ics2021 <tracer@njuics.org>
2021-11-15 23:13:28 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 36e142ead4c74344ad2272f6ff423232     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:13:28 up  3:40,  1 user,  load average: 0.10, 0.13, 0.16

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,17 +1,35 @@
 #include <common.h>
 
 static Context* do_event(Event e, Context* c) {
-  switch (e.event) {
-	case EVENT_YIELD:
-		printf("YILED!\n");
-		break;
-    default: panic("Unhandled event ID = %d", e.event);
-  }
+	switch (e.event) {
+		case EVENT_NULL:
+			Log("Unhandled EVENT_YILED!\n");
+			break;
+		case EVENT_YIELD: 
+			Log("Unhandled EVENT_YIELD!\n");
+			break;
+		case EVENT_SYSCALL: 
+			Log("Unhandled EVENT_SYSCALL!\n");
+			break;
+		case EVENT_PAGEFAULT: 
+			Log("Unhandled EVENT_PAGEFAULT!\n");
+			break;
+		case EVENT_ERROR:
+			Log("Unhandled EVENT_ERROR!\n");
+			break;
+		case EVENT_IRQ_TIMER:
+			Log("Unhandled EVENT_IRQ_TIMER\n");
+			break;
+		case EVENT_IRQ_IODEV:
+			Log("Unhandled EVENT_IRQ_IODEV!\n");
+			break;
+		default: panic("Unhandled event ID = %d", e.event);
+	}
 
-  return c;
+	return c;
 }
 
 void init_irq(void) {
-  Log("Initializing interrupt/exception handler...");
-  cte_init(do_event);
+	Log("Initializing interrupt/exception handler...");
+	cte_init(do_event);
 }
