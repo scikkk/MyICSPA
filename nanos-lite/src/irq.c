656e77759ac838d4da142cda1928038d58ef377d
tracer-ics2021 <tracer@njuics.org>
2021-11-16 00:05:40 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 36e142ead4c74344ad2272f6ff423232     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:05:40 up  4:32,  1 user,  load average: 0.00, 0.04, 0.07

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,26 +4,26 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
 	switch (e.event) {
 		case EVENT_NULL:
-			Log("Unhandled EVENT_YILED!\n");
+			Log("\nUnhandled EVENT_YILED!\n");
 			break;
 		case EVENT_YIELD: 
-			Log("Unhandled EVENT_YIELD!\n");
+			Log("\nUnhandled EVENT_YIELD!\n");
 			break;
 		case EVENT_SYSCALL: 
-			/* Log("Unhandled EVENT_SYSCALL!\n"); */
+			/* Log("\nUnhandled EVENT_SYSCALL!\n"); */
 			do_syscall(c);
 			break;
 		case EVENT_PAGEFAULT: 
-			Log("Unhandled EVENT_PAGEFAULT!\n");
+			Log("\nUnhandled EVENT_PAGEFAULT!\n");
 			break;
 		case EVENT_ERROR:
-			Log("Unhandled EVENT_ERROR!\n");
+			Log("\nUnhandled EVENT_ERROR!\n");
 			break;
 		case EVENT_IRQ_TIMER:
-			Log("Unhandled EVENT_IRQ_TIMER\n");
+			Log("\nUnhandled EVENT_IRQ_TIMER\n");
 			break;
 		case EVENT_IRQ_IODEV:
-			Log("Unhandled EVENT_IRQ_IODEV!\n");
+			Log("\nUnhandled EVENT_IRQ_IODEV!\n");
 			break;
 		default: panic("Unhandled event ID = %d", e.event);
 	}
