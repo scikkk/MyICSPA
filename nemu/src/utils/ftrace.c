5ab408332cc27e0d2a1993665d123e7566079a49
tracer-ics2021 <tracer@njuics.org>
2021-11-15 17:43:44 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 8bdd46ee5bf643e58aadd6eff1875ed3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  17:43:44 up  1:04,  1 user,  load average: 0.19, 0.20, 0.13

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -18,7 +18,7 @@ static struct FtraceOneline{
 	paddr_t pc;
 	unsigned name_idx;
 	paddr_t dst;
-} ftrace_res[10000];
+} ftrace_res[65535];
 static unsigned ftrace_idx = 0;
 
 static void tableheader(const char *pbuff)
