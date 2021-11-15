abdd6fc6316b8cbe852e60a99ad8a7231b385f8e
tracer-ics2021 <tracer@njuics.org>
2021-11-16 00:03:41 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 36e142ead4c74344ad2272f6ff423232     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:03:41 up  4:30,  1 user,  load average: 0.02, 0.07, 0.09

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -1,16 +1,18 @@
 #include <isa.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
-   * Then return the address of the interrupt/exception vector.
-   */
-  printf("ERROR: NO:%d;\tepc:0x%08x\n", NO, epc);
-  cpu.mepc = epc;
-  cpu.mcause = NO;
-  isa_reg_display();
-  return cpu.mtvec;
+	/* TODO: Trigger an interrupt/exception with ``NO''.
+	 * Then return the address of the interrupt/exception vector.
+	 */
+#ifdef CONFIG_ETRACE
+	printf("ERROR: NO:%d;\tepc:0x%08x\n", NO, epc);
+	isa_reg_display();
+#endif
+	cpu.mepc = epc;
+	cpu.mcause = NO;
+	return cpu.mtvec;
 }
 
 word_t isa_query_intr() {
-  return INTR_EMPTY;
+	return INTR_EMPTY;
 }
