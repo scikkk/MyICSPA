ba53aef8902e35a850d5111ebe7414a4d6d561a4
tracer-ics2021 <tracer@njuics.org>
2021-11-15 12:23:03 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 06f84a70496d4064a781ba477e36b05c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:23:03 up 2 min,  1 user,  load average: 0.38, 0.50, 0.23

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -8,8 +8,8 @@ void init_fs(void);
 void init_proc(void);
 
 int main() {
-	/* extern const char logo[]; */
-	char logo[] = "WANGKE\n";
+	extern const char logo[];
+	/* char logo[] = "WANGKE\n"; */
 	printf("%s\n", logo);
 	Log("'Hello World!' from Nanos-lite");
 	Log("Build time: %s, %s", __TIME__, __DATE__);
