ccd3b318c691f0c8ceb7231dd88f81219d9a3fa6
tracer-ics2021 <tracer@njuics.org>
2021-11-16 20:10:10 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e5e7c2c3058f498bbf6ae5a06e1bdabb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:10:10 up 40 min,  1 user,  load average: 0.31, 0.20, 0.13

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -106,7 +106,7 @@ void init_ftrace(const char *elf_file) {
 	fclose(ramdisk_fp);
 	tableheader(elf_str);
 	free(elf_str);
-	Log("Symbol table is loaded from %s", elf_file);
+	Log("Symbol table is loaded from %s", "/home/wk/ics2021/nanos-lite/build/ramdisk.img");
 	// wk 3.2 ---------------------------------------------------------
 
 	/* func_display(); */
