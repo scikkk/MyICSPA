6377c54bd301b88a2958797730ec4591ccd54976
tracer-ics2021 <tracer@njuics.org>
2021-11-16 22:49:57 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e5e7c2c3058f498bbf6ae5a06e1bdabb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  22:49:57 up  1:19,  1 user,  load average: 0.01, 0.05, 0.07

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,79 +1,80 @@
 #include <common.h>
 #include "syscall.h"
 
-
+#ifdef CONFIG_STRACE 
 void strace(Context *c) {
-    char type[20];
-    uintptr_t a[4];
-    a[0] = c->GPR1;
-    a[1] = c->GPR2;
-    a[2] = c->GPR3;
-    a[3] = c->GPR4;
+	char type[20];
+	uintptr_t a[4];
+	a[0] = c->GPR1;
+	a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
 
-    switch (a[0]) {
-    case SYS_exit:
-        strcpy(type, "SYS_exit");
-        break;
-    case SYS_yield:
-        strcpy(type, "SYS_yield");
-        break;
-    case SYS_open:
-        strcpy(type, "SYS_open");
-        break;
-    case SYS_read:
-        strcpy(type, "SYS_read");
-        break;
-    case SYS_write:
-        strcpy(type, "SYS_write");
-        break;
-    case SYS_kill:
-        strcpy(type, "SYS_kill");
-        break;
-    case SYS_getpid:
-        strcpy(type, "SYS_getpid");
-        break;
-    case SYS_close:
-        strcpy(type, "SYS_close");
-        break;
-    case SYS_lseek:
-        strcpy(type, "SYS_lseek");
-        break;
-    case SYS_brk:
-        strcpy(type, "SYS_brk");
-        break;
-    case SYS_fstat:
-        strcpy(type, "SYS_fstat");
-        break;
-    case SYS_time:
-        strcpy(type, "SYS_time");
-        break;
-    case SYS_signal:
-        strcpy(type, "SYS_signal");
-        break;
-    case SYS_execve:
-        strcpy(type, "SYS_execve");
-    }
+	switch (a[0]) {
+		case SYS_exit:
+			strcpy(type, "SYS_exit");
+			break;
+		case SYS_yield:
+			strcpy(type, "SYS_yield");
+			break;
+		case SYS_open:
+			strcpy(type, "SYS_open");
+			break;
+		case SYS_read:
+			strcpy(type, "SYS_read");
+			break;
+		case SYS_write:
+			strcpy(type, "SYS_write");
+			break;
+		case SYS_kill:
+			strcpy(type, "SYS_kill");
+			break;
+		case SYS_getpid:
+			strcpy(type, "SYS_getpid");
+			break;
+		case SYS_close:
+			strcpy(type, "SYS_close");
+			break;
+		case SYS_lseek:
+			strcpy(type, "SYS_lseek");
+			break;
+		case SYS_brk:
+			strcpy(type, "SYS_brk");
+			break;
+		case SYS_fstat:
+			strcpy(type, "SYS_fstat");
+			break;
+		case SYS_time:
+			strcpy(type, "SYS_time");
+			break;
+		case SYS_signal:
+			strcpy(type, "SYS_signal");
+			break;
+		case SYS_execve:
+			strcpy(type, "SYS_execve");
+	}
 	printf("TYPE: %s(%p)\ta0:%p\ta1:%p\ta2:%p\n", type, a[0],a[1],a[2],a[3]);
 }
-
+#endif 
 void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
-  a[1] = c->GPR2;
-  a[2] = c->GPR3;
-  a[3] = c->GPR4;
+	uintptr_t a[4];
+	a[0] = c->GPR1;
+	a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
 
 
-  switch (a[0]) {
-	  case SYS_yield:
-		  sys_yield();
-		  break;
-	  case SYS_exit:
-		  /* printf("%d\t%d\t%d\t%d\n\n", a[0],a[1],a[2],a[3]); */
-		  sys_exit(a[1]);
-		  break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
-  }
-
-  strace(c);
+	switch (a[0]) {
+		case SYS_yield:
+			sys_yield();
+			break;
+		case SYS_exit:
+			/* printf("%d\t%d\t%d\t%d\n\n", a[0],a[1],a[2],a[3]); */
+			sys_exit(a[1]);
+			break;
+		default: panic("Unhandled syscall ID = %d", a[0]);
+	}
+#ifdef CONFIG_STRACE 
+	strace(c);
+#endif
 }
