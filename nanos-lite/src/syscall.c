098d11b89f76cc5044c979481b40339e48cb3c02
tracer-ics2021 <tracer@njuics.org>
2021-11-16 22:36:11 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e5e7c2c3058f498bbf6ae5a06e1bdabb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  22:36:11 up  1:05,  1 user,  load average: 0.11, 0.13, 0.11

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,60 @@
 #include <common.h>
 #include "syscall.h"
+
+
+void strace(Context *c) {
+    char type[20];
+    uintptr_t a[4];
+    a[0] = c->GPR1;
+    a[1] = c->GPR2;
+    a[2] = c->GPR3;
+    a[3] = c->GPR4;
+
+    switch (a[0]) {
+    case SYS_exit:
+        strcpy(type, "SYS_exit");
+        break;
+    case SYS_yield:
+        strcpy(type, "SYS_yield");
+        break;
+    case SYS_open:
+        strcpy(type, "SYS_open");
+        break;
+    case SYS_read:
+        strcpy(type, "SYS_read");
+        break;
+    case SYS_write:
+        strcpy(type, "SYS_write");
+        break;
+    case SYS_kill:
+        strcpy(type, "SYS_kill");
+        break;
+    case SYS_getpid:
+        strcpy(type, "SYS_getpid");
+        break;
+    case SYS_close:
+        strcpy(type, "SYS_close");
+        break;
+    case SYS_lseek:
+        strcpy(type, "SYS_lseek");
+        break;
+    case SYS_brk:
+        strcpy(type, "SYS_brk");
+        break;
+    case SYS_fstat:
+        strcpy(type, "SYS_fstat");
+        break;
+    case SYS_time:
+        strcpy(type, "SYS_time");
+        break;
+    case SYS_signal:
+        strcpy(type, "SYS_signal");
+        break;
+    case SYS_execve:
+        strcpy(type, "SYS_execve");
+    }
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -18,4 +73,6 @@ void do_syscall(Context *c) {
 		  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  strace(c);
 }
