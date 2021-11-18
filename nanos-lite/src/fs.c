7963ada3ea9d8f5c88e38ca7b49e77e735d8eb03
tracer-ics2021 <tracer@njuics.org>
2021-11-19 00:05:03 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: ae5801f5a4384d0e9bb1a6ebe3661d53     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:05:03 up 33 min,  1 user,  load average: 0.07, 0.17, 0.15

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -79,6 +79,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
 
 size_t fs_lseek(int fd, size_t offset, int whence){
 	/* assert(fd > 2); */	
+	printf("%d\t%d\t%d\n", SEEK_END);
 	switch (whence){
 	 	case SEEK_SET: 
 			open_offset[fd] = offset;
