e594491a8fd564469f29be6df8e74257cf108620
tracer-ics2021 <tracer@njuics.org>
2021-11-19 20:06:04 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b40ceb11de4a4cea99e0bfd0ba1b863b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:06:04 up 51 min,  1 user,  load average: 0.12, 0.13, 0.14

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -8,30 +8,30 @@ int main() {
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   printf("%d\n", size);
-  /* assert(size == 5000); */
+  assert(size == 5000);
 
-  fseek(fp, 500 * 5, SEEK_SET);
-  int i, n;
-  for (i = 500; i < 1000; i ++) {
-    fscanf(fp, "%d", &n);
+  /* fseek(fp, 500 * 5, SEEK_SET); */
+  /* int i, n; */
+  /* for (i = 500; i < 1000; i ++) { */
+  /*   fscanf(fp, "%d", &n); */
     /* assert(n == i + 1); */
-  }
+  /* } */
 
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
-    fprintf(fp, "%4d\n", i + 1 + 1000);
-  }
+  /* fseek(fp, 0, SEEK_SET); */
+  /* for (i = 0; i < 500; i ++) { */
+    /* fprintf(fp, "%4d\n", i + 1 + 1000); */
+  /* } */
 
-  for (i = 500; i < 1000; i ++) {
-    fscanf(fp, "%d", &n);
+  /* for (i = 500; i < 1000; i ++) { */
+    /* fscanf(fp, "%d", &n); */
     /* assert(n == i + 1); */
-  }
+  /* } */
 
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
-    fscanf(fp, "%d", &n);
+  /* fseek(fp, 0, SEEK_SET); */
+  /* for (i = 0; i < 500; i ++) { */
+    /* fscanf(fp, "%d", &n); */
     /* assert(n == i + 1 + 1000); */
-  }
+  /* } */
 
   fclose(fp);
 
