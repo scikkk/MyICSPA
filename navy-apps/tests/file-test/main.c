d059c8e8ac9d183fcc6e8b082ddeb03b0de4b09a
tracer-ics2021 <tracer@njuics.org>
2021-11-19 20:39:58 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b40ceb11de4a4cea99e0bfd0ba1b863b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:39:58 up  1:25,  1 user,  load average: 0.17, 0.10, 0.09

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -12,10 +12,11 @@ int main() {
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
-  /* for (i = 500; i < 1000; i ++) { */
-  /*   fscanf(fp, "%d", &n); */
-  /*   assert(n == i + 1); */
-  /* } */
+  for (i = 500; i < 1000; i ++) {
+    fscanf(fp, "%d", &n);
+	printf("i=%d\tn=%d\n", i, n);
+    assert(n == i + 1);
+  }
 
   fseek(fp, 0, SEEK_SET);
   /* for (i = 0; i < 500; i ++) { */
@@ -27,11 +28,11 @@ int main() {
   /*   assert(n == i + 1); */
   /* } */
 
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1 + 1000);
-  }
+  /* fseek(fp, 0, SEEK_SET); */
+  /* for (i = 0; i < 500; i ++) { */
+  /*   fscanf(fp, "%d", &n); */
+  /*   assert(n == i + 1 + 1000); */
+  /* } */
 
   fclose(fp);
 
