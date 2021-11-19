c2b65a1f9715323d96a0e5362fdd689359f2ef80
tracer-ics2021 <tracer@njuics.org>
2021-11-19 19:26:02 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b40ceb11de4a4cea99e0bfd0ba1b863b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:26:02 up 11 min,  1 user,  load average: 0.10, 0.12, 0.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,9 +27,9 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/dummy");
+  /* naive_uload(NULL, "/bin/dummy"); */
   /* naive_uload(NULL, "/bin/hello"); */
-  /* naive_uload(NULL, "/bin/file-test"); */
+  naive_uload(NULL, "/bin/file-test");
   /* naive_uload(NULL, "/bin/exec-test"); */
   /* naive_uload(NULL, "/bin/event-test"); */
   /* naive_uload(NULL, "/bin/cpp-test"); */
