051ad21849f608c6f3c50e8b52d161fcd91017d0
tracer-ics2021 <tracer@njuics.org>
2021-11-19 23:33:53 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: fe77cdbaf7214ca18aa0ba5ef1a7a250     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:33:53 up  1:20,  1 user,  load average: 0.20, 0.12, 0.08

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -7,44 +7,35 @@ int main() {
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-  printf("%d\n", size);
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-	printf("i=%d\tn=%d\n", i, n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
-  size = ftell(fp);
-  printf("size: %d\n", size);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  
-  fseek(fp, 500 * 5, SEEK_SET);
-  size = ftell(fp); 
-   printf("size: %d\n", size); 
-  for (i = 500; i < 1000; i ++) { // 1000
+  for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-	printf("i=%d\tn=%d\n", i, n);
     assert(n == i + 1);
   }
+
   fseek(fp, 0, SEEK_SET);
-   size = ftell(fp); 
-   printf("size: %d\n", size); 
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
-	 printf("i=%d\tn=%d\n", i, n); 
     assert(n == i + 1 + 1000);
   }
+
   fclose(fp);
 
   printf("PASS!!!\n");
 
   return 0;
 }
+
