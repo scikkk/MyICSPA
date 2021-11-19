9cf9a6f42f9d1a2422438a3cd77b96c97cc73407
tracer-ics2021 <tracer@njuics.org>
2021-11-19 19:53:12 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b40ceb11de4a4cea99e0bfd0ba1b863b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:53:12 up 39 min,  1 user,  load average: 0.27, 0.23, 0.19

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -7,7 +7,7 @@ int main() {
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-  printf("%d\n", size);
+  printf("%d\n", fp);
   /* assert(size == 5000); */
 
   /* fseek(fp, 500 * 5, SEEK_SET); */
