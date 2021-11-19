1aebd4b4e82425fb14814d18281687db9d3987f9
tracer-ics2021 <tracer@njuics.org>
2021-11-19 21:34:20 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b40ceb11de4a4cea99e0bfd0ba1b863b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  21:34:20 up  2:20,  1 user,  load average: 0.09, 0.09, 0.09

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,7 +82,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
 	len = (len>max_len)?max_len:len;
 	
 	ramdisk_write(buf, file_table[fd].disk_offset+open_offset[fd], len);
-	open_offset[fd] += len+1;
+	open_offset[fd] += len;
 	assert(open_offset[fd] <= file_table[fd].size);
 	return len; 
 }
