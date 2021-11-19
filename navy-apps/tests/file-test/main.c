81b3e3fe88854f12582a103a5631a30ecf6c7c5e
tracer-ics2021 <tracer@njuics.org>
2021-11-19 21:23:51 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b40ceb11de4a4cea99e0bfd0ba1b863b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  21:23:51 up  2:09,  1 user,  load average: 0.04, 0.13, 0.11

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -14,7 +14,7 @@ int main() {
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-	/* printf("i=%d\tn=%d\n", i, n); */
+	printf("i=%d\tn=%d\n", i, n);
     assert(n == i + 1);
   }
 
@@ -25,12 +25,14 @@ int main() {
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+	printf("i=%d\tn=%d\n", i, n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
+	printf("i=%d\tn=%d\n", i, n);
     assert(n == i + 1 + 1000);
   }
 
