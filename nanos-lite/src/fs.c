aa70b597d4e07353fa989b44a554f1c651f3753d
tracer-ics2021 <tracer@njuics.org>
2021-11-19 20:42:20 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b40ceb11de4a4cea99e0bfd0ba1b863b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:42:20 up  1:28,  1 user,  load average: 0.15, 0.15, 0.11

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -65,11 +65,11 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
 assert(fd > 2);
 	ramdisk_read(buf, file_table[fd].disk_offset+open_offset[fd], len);
+	assert(open_offset[fd] <= file_table[fd].size);
 
 	printf("openoff: %d; size: %d", open_offset[fd], file_table[fd].size);
 	open_offset[fd] += len;
 	printf("openoff: %d; size: %d", open_offset[fd], file_table[fd].size);
-	assert(open_offset[fd] <= file_table[fd].size);
 	return len;
 }
 size_t fs_write(int fd, const void *buf, size_t len){
