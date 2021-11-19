7f291a8289e135227bf1ca7911c74e73923ddb6e
tracer-ics2021 <tracer@njuics.org>
2021-11-19 23:50:30 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: fe77cdbaf7214ca18aa0ba5ef1a7a250     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:50:30 up  1:37,  1 user,  load average: 0.18, 0.07, 0.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -78,9 +78,11 @@ size_t fs_read(int fd, void *buf, size_t len){
 }
 size_t fs_write(int fd, const void *buf, size_t len){
 	assert(fd > 2);
+
 	size_t max_len = file_table[fd].size - open_offset[fd];
 	/* printf("maxlen: %d\t len: %d\n", max_len, len); */
 	len = (len>max_len)?max_len:len;
+	len = (strlen((char*)buf) < len)?strlen((char*)buf):len;
 	/* printf("write:%d\t%d\t%d\n",open_offset[fd], len, open_offset[fd]+len); */
 	ramdisk_write(buf, file_table[fd].disk_offset+open_offset[fd], len);
 	open_offset[fd] += len;
