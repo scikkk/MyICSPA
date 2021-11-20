c089e36236d8ca78a837f8d30bb7f58bb2cc6116
tracer-ics2021 <tracer@njuics.org>
2021-11-20 09:18:32 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: ca3128887ecc4463939f0e04e1ddf71c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  09:18:32 up 15 min,  1 user,  load average: 0.09, 0.11, 0.14

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,22 +64,26 @@ int fs_open(const char *pathname, int flags, int mode){
 
 size_t fs_read(int fd, void *buf, size_t len){
 	assert(fd > 2);
+	printf("%d: offset: %d\n", __LINE__, file_table[fd].open_offset);
 	if(file_table[fd].open_offset >= file_table[fd].size || len == 0)
 		return 0;
 	if(file_table[fd].open_offset + len > file_table[fd].size)
 		len = file_table[fd].size - file_table[fd].open_offset;
 	ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
+	printf("%d: offset: %d\n", __LINE__, file_table[fd].open_offset);
 	return len;
 }
 size_t fs_write(int fd, const void *buf, size_t len){
 	assert(fd > 2);
+	printf("%d: offset: %d\n", __LINE__, file_table[fd].open_offset);
 	if(file_table[fd].open_offset >= file_table[fd].size)
 		return 0;
 	if(file_table[fd].open_offset + len > file_table[fd].size)
 		len = file_table[fd].size - file_table[fd].open_offset;
 	ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
+	printf("%d: offset: %d\n", __LINE__, file_table[fd].open_offset);
 	return len; 
 }
 
