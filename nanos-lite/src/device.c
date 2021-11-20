b33ec2b13ca66f98131a979944b77f3ce3fa5604
tracer-ics2021 <tracer@njuics.org>
2021-11-20 16:31:14 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 31a2151eea48479f83442db01cd7db7a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:31:14 up  2:06,  1 user,  load average: 0.49, 0.28, 0.15

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,6 +23,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
 	assert(offset == 0);
+	assert(0);
 	char tmp[50];
 	/* while (1) { */
 		AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
