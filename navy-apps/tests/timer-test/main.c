9aa83601bd01f75dd0cd9a37f62448d6bfb19c25
tracer-ics2021 <tracer@njuics.org>
2021-11-20 15:04:58 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 31a2151eea48479f83442db01cd7db7a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  15:04:58 up 39 min,  1 user,  load average: 0.07, 0.08, 0.08

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -11,30 +11,50 @@ extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 void hello_world(void)
 {
-    printf("Hello world!!!!\r\n");
+	printf("Hello world!!!!\r\n");
 }
 
 int main(void)
 {
 
-        struct  timeval   tv_begin,tv_end;
-        
-//        gettimeofday(&tv_begin,NULL);
-		_syscall_(SYS_gettimeofday, (unsigned long)&tv_begin, 0, 0);
-        
-        hello_world();
+	struct  timeval   tv_begin,tv_end, tv_cur;
 
-//        gettimeofday(&tv_end,NULL);
-		_syscall_(SYS_gettimeofday, (unsigned long)&tv_begin, 0, 0);
-    
-        printf("tv_begin_sec:%ld\n",tv_begin.tv_sec);
+	//        gettimeofday(&tv_begin,NULL);
+	_syscall_(SYS_gettimeofday, (unsigned long)&tv_begin, 0, 0);
 
-        printf("tv_begin_usec:%ld\n",tv_begin.tv_usec);
+	hello_world();
 
-        printf("tv_end_sec:%ld\n",tv_end.tv_sec);
+	//        gettimeofday(&tv_end,NULL);
+	_syscall_(SYS_gettimeofday, (unsigned long)&tv_begin, 0, 0);
 
-        printf("tv_end_usec:%ld\n",tv_end.tv_usec);
+	printf("tv_begin_sec:%ld\n",tv_begin.tv_sec);
 
-        return 0;
+	printf("tv_begin_usec:%ld\n",tv_begin.tv_usec);
+
+	printf("tv_end_sec:%ld\n",tv_end.tv_sec);
+
+	printf("tv_end_usec:%ld\n",tv_end.tv_usec);
+
+
+
+
+
+	int sec = 1;
+	while (sec<6) {
+		while(1){
+		
+	_syscall_(SYS_gettimeofday, (unsigned long)&tv_cur, 0, 0);
+	if(tv_cur.tv_sec > sec) break;
+		} 
+		
+		printf("(sec.usec): %lu.%lu\n",tv_cur.tv_sec, tv_cur.tv_usec);
+		if (sec == 1) {
+			printf("%d second).\n", sec);
+		} else {
+			printf("%d seconds).\n", sec);
+		}
+		sec ++;
+	}
+	return 0;
 
 }
