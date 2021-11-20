3bbf889933fd96fbe38ff19f288da0950b0cd256
tracer-ics2021 <tracer@njuics.org>
2021-11-20 16:32:45 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 31a2151eea48479f83442db01cd7db7a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:32:45 up  2:07,  1 user,  load average: 0.30, 0.27, 0.16

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -73,6 +73,9 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
 	assert(fd > 2);
 	/* printf("%d: offset: %d\n", __LINE__, file_table[fd].open_offset); */
+	if(file_table[fd].read != NULL){
+		return file_table[fd].read(buf, 0, len);
+	}
 	if(file_table[fd].open_offset >= file_table[fd].size || len == 0)
 		return 0;
 	if(file_table[fd].open_offset + len > file_table[fd].size)
