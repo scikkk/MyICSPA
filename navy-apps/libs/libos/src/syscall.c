27794fc4565c8509992412f9978df60d852d70d3
tracer-ics2021 <tracer@njuics.org>
2021-11-20 11:08:19 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: ca3128887ecc4463939f0e04e1ddf71c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  11:08:19 up  2:05,  1 user,  load average: 0.20, 0.14, 0.11

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -63,8 +63,8 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
 	/* _exit(SYS_write); */
-	_syscall_(SYS_write, fd, (int)buf, count);
-	return 0;
+	return _syscall_(SYS_write, fd, (int)buf, count);
+	/* return 0; */
 }
 
 
