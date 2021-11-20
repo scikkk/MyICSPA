903e9e3d6a9018e22995c6f14820fe4d529042b2
tracer-ics2021 <tracer@njuics.org>
2021-11-20 10:43:09 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: ca3128887ecc4463939f0e04e1ddf71c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  10:43:09 up  1:40,  1 user,  load average: 0.44, 0.30, 0.20

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,14 +3,14 @@
 
 int main() {
   write(1, "Hello\n World!\n", 13);
-  int i = 2;
-  volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from\n Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    }
-  }
+  /* int i = 2; */
+  /* volatile int j = 0; */
+  /* while (1) { */
+  /*   j ++; */
+  /*   if (j == 10000) { */
+  /*     printf("Hello World from Navy-apps for the %dth time!\n", i ++); */
+  /*     j = 0; */
+  /*   } */
+  /* } */
   return 0;
 }
