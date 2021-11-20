190a4ce5238d77a826d24c411c6efc9523488d38
tracer-ics2021 <tracer@njuics.org>
2021-11-20 20:02:07 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 9e3ab68ac54d46a89c4088d0845cda60     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:02:07 up  1:04,  1 user,  load average: 0.25, 0.21, 0.10

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -78,7 +78,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 	assert(fd > 2);
 	/* printf("%d: offset: %d\n", __LINE__, file_table[fd].open_offset); */
 	if(file_table[fd].read != NULL){
-		return file_table[fd].read(buf, 0, len);
+		return file_table[fd].read(buf, file_table[fd].open_offset, len);
 	}
 	if(file_table[fd].open_offset >= file_table[fd].size || len == 0)
 		return 0;
@@ -91,7 +91,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 }
 size_t fs_write(int fd, const void *buf, size_t len){
 	if(file_table[fd].write != NULL){
-		return file_table[fd].write(buf, 0, len);
+		return file_table[fd].write(buf, file_table[fd].open_offset, len);
 	}
 	assert(fd > 2);
 	/* printf("%d: offset: %d\n", __LINE__, file_table[fd].open_offset); */
