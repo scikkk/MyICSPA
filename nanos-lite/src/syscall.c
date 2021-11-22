22157bae8891a13c0556e0c80b6c1fe3de8ba8e7
tracer-ics2021 <tracer@njuics.org>
2021-11-22 16:15:01 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 4a8e3a3e14eb454f990942985d6ec39d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:15:01 up 1 min,  1 user,  load average: 0.91, 0.45, 0.17

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -93,9 +93,9 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
 		return -1;
 	}
 	uint64_t us = io_read(AM_TIMER_UPTIME).us;
-	tv->tv_sec = (int)us / 1000000;
+	tv->tv_sec = (long)us / 1000000;
 	printf("sys_sec: %d\n", (int)tv->tv_sec);
-	tv->tv_usec = (int)us % 1000000;
+	tv->tv_usec = (long)us % 1000000;
 	printf("sys_usec: %d\n", (int)tv->tv_usec);
 
 	/* printf("%d\t%d\n", tv->tv_sec, tv->tv_usec); */
