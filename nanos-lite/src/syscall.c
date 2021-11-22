5670774ccc8310966c41733b7d3ca80bc8699e74
tracer-ics2021 <tracer@njuics.org>
2021-11-22 16:41:55 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 4a8e3a3e14eb454f990942985d6ec39d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:41:55 up 28 min,  1 user,  load average: 0.05, 0.23, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,29 +1,38 @@
 #include <common.h>
 
+// wk
+#include <unistd.h>
+#include <sys/stat.h>
+#include <setjmp.h>
+#include <sys/time.h>
+/* #include <assert.h> */
+#include <time.h>
+#include "syscall.h"
+// wk
 
 
-enum {
-	SYS_exit,
-	SYS_yield,
-	SYS_open,
-	SYS_read,
-	SYS_write,
-	SYS_kill,
-	SYS_getpid,
-	SYS_close,
-	SYS_lseek,
-	SYS_brk,
-	SYS_fstat,
-	SYS_time,
-	SYS_signal,
-	SYS_execve,
-	SYS_fork,
-	SYS_link,
-	SYS_unlink,
-	SYS_wait,
-	SYS_times,
-	SYS_gettimeofday
-};
+/* enum { */
+/* 	SYS_exit, */
+/* 	SYS_yield, */
+/* 	SYS_open, */
+/* 	SYS_read, */
+/* 	SYS_write, */
+/* 	SYS_kill, */
+/* 	SYS_getpid, */
+/* 	SYS_close, */
+/* 	SYS_lseek, */
+/* 	SYS_brk, */
+/* 	SYS_fstat, */
+/* 	SYS_time, */
+/* 	SYS_signal, */
+/* 	SYS_execve, */
+/* 	SYS_fork, */
+/* 	SYS_link, */
+/* 	SYS_unlink, */
+/* 	SYS_wait, */
+/* 	SYS_times, */
+/* 	SYS_gettimeofday */
+/* }; */
 
 int sys_yield(){
 	yield();
