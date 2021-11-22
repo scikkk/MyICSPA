368936ed9f033eec1dcff5a9046edf4d14466ad3
tracer-ics2021 <tracer@njuics.org>
2021-11-22 23:02:37 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 77a7272e2f774e4fb8db65522f7b8d95     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:02:37 up 36 min,  1 user,  load average: 0.12, 0.21, 0.14

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -27,69 +27,90 @@ int SDL_WaitEvent(SDL_Event *event) {
 			char type[3], name[20];
 			sscanf(buf, "%s %s\n", type, name);
 			if(strcmp(type, "ku") == 0){
+				printf("%d\n", __LINE__);
 				event->type = SDL_KEYUP;
 			}
 			else if(strcmp(type, "kd")){
+				printf("%d\n", __LINE__);
 				event->type = SDL_KEYDOWN;
 			}
 			else{
+				printf("%d\n", __LINE__);
 				return 0;
 			}
 			if(!strcmp(name, "0")){
+				printf("%d\n", __LINE__);
 
 				event->key.keysym.sym = SDLK_0;
 			}
 			else if(!stramp(name, "1")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_1; 
 			}
 			else if(!stramp(name, "2")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_2; 
 			}
 			else if(!stramp(name, "3")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_3; 
 			}
 			else if(!stramp(name, "4")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_4; 
 			}
 			else if(!stramp(name, "5")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_5; 
 			}
 			else if(!stramp(name, "6")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_6;
 			}
 			else if(!stramp(name, "7")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_7; 
 			}
 			else if(!stramp(name, "8")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_8;
 			}
 			else if(!stramp(name, "9")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_9; 
 			}
 			else if(!stramp(name, "J")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_J; 
 			}
 			else if(!stramp(name, "DOWN")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_DOWN; 
 			}
 			else if(!stramp(name, "K")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_K; 
 			}
 			else if(!stramp(name, "UP")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_UP; 
 			}
 			else if(!stramp(name, "G")){
+				printf("%d\n", __LINE__);
 				event->key.keysym.sym = SDLK_G; 
 			}
 			else{
+				printf("%d\n", __LINE__);
 				return 0;
 			}
 			/* printf("%s\n", type); */
 			/* printf("%s\n", name); */
+			printf("%d\n", __LINE__);
 
 			return 1;
 		}
 	}
+	printf("%d\n", __LINE__);
 	return 0;
 }
 
