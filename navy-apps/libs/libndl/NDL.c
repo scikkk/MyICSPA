a70e94ccf59043d6ff4ab1d5176e7d154aac8cc9
tracer-ics2021 <tracer@njuics.org>
2021-11-22 16:22:32 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 4a8e3a3e14eb454f990942985d6ec39d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:22:32 up 9 min,  1 user,  load average: 0.14, 0.18, 0.15

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -20,8 +20,8 @@ uint32_t NDL_GetTicks() {
 	printf("hit there\n");
 	uint32_t ret = tv.tv_sec*1000+tv.tv_usec/1000;
 
-	printf("NDL_sec: %d\n", tv.tv_sec);
-	printf("NDL_usec: %d\n", tv.tv_usec);
+	printf("NDL_sec: %ld\n", tv.tv_sec);
+	printf("NDL_usec: %ld\n", tv.tv_usec);
 	printf("NDL_ms: %d\n", ret);
 	return ret;
 
