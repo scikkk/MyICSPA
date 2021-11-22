ab445fb99ae7556d55d6e4c10472838eec1de683
tracer-ics2021 <tracer@njuics.org>
2021-11-22 21:43:19 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: de9c4c0ac23f4e7ab556a9216d56271b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  21:43:19 up 35 min,  1 user,  load average: 0.08, 0.19, 0.22

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -83,7 +83,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 	for(int cur_y = y; cur_y < y + h; cur_y++){
 		offset = (screen_w*cur_y + x)*4;
 		fseek(fp, offset, SEEK_SET);
-		printf("NDL:x=%d\ty=%d\toffset=%d\n", x, cur_y, offset);
+		printf("NDL:x=%d y=%d offset=%d\n", x, cur_y, offset);
 		fwrite(pixels, 4, w, fp);
 		pixels += w;
 	}
