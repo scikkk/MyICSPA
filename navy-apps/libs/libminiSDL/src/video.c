03bc4f8f97743268d9297f680e80b98518fe9189
tracer-ics2021 <tracer@njuics.org>
2021-11-25 08:47:02 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 15c34a91d8134e3d99f41554a0cfc97d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  08:47:02 up 7 min,  1 user,  load average: 0.11, 0.33, 0.21

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -10,14 +10,14 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-	printf("\n\nvideoo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	printf("\n\nvideo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 	
 	NDL_DrawRect(s, x, y, w, h);
 	
-	printf("\n\nvideoo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	printf("\n\nvideo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
 }
 
 // APIs below are already implemented.
@@ -198,10 +198,10 @@ uint32_t SDL_MapRGBA(SDL_PixelFormat *fmt, uint8_t r, uint8_t g, uint8_t b, uint
 }
 
 int SDL_LockSurface(SDL_Surface *s) {
-	printf("\n\nvideoo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	printf("\n\nvideo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
   return 0;
 }
 
 void SDL_UnlockSurface(SDL_Surface *s) {
-	printf("\n\nvideoo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	printf("\n\nvideo.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
 }
