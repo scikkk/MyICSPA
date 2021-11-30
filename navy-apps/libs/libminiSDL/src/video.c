d6a6e6d69f2e965b814358f155470d4787a44f40
tracer-ics2021 <tracer@njuics.org>
2021-11-30 23:23:38 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 1d999f810535485e90541e66f4a573e1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:23:38 up  3:28,  1 user,  load average: 0.07, 0.09, 0.12

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -25,13 +25,13 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	dx = dstrect->x;
 	dy = dstrect->y;
 	uint32_t *srcpixels, *dstpixels, srcoffset, dstoffset;
-	printf("asdf\n");
 	for(int cur_h = 0; cur_h < h; cur_h++){
 		srcoffset = w*(sy + cur_h) + sx;
 		*srcpixels = (uint32_t*)src->pixels + srcoffset;
 		dstoffset = w*(dy + cur_h) + dx;
 		*srcpixels = (uint32_t*)dst->pixels + dstoffset;
 		for(int k = 0; k < w; k++){
+	printf("asdf\n");
 			*((uint32_t*)dstpixels+k) = *((uint32_t*)srcpixels+k);
 		}
 
