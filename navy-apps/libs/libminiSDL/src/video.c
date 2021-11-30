fecaeb09ed93ea6addb20621b59c94d067a833fc
tracer-ics2021 <tracer@njuics.org>
2021-11-30 23:21:33 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 1d999f810535485e90541e66f4a573e1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:21:33 up  3:26,  1 user,  load average: 0.07, 0.10, 0.13

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -7,6 +7,34 @@
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
 	assert(dst && src);
 	assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
+	int dx,dy;
+	int sx,sy,w,h;
+
+	if(srcrect == NULL){
+		sx = 0;
+		sy = 0;
+		w = src->w;
+		h = src->h;
+	}
+	else{
+		sx = srcrect->x;
+		sy = srcrect->y;
+		w = srcrect->w;
+		h = srcrect->h;
+	}
+	dx = dstrect->x;
+	dy = dstrect->y;
+	uint32_t *srcpixels, *dstpixels, srcoffset, dstoffset;
+	for(int cur_h = 0; cur_h < h; cur_h++){
+		srcoffset = w*(sy + cur_h) + sx;
+		*srcpixels = (uint32_t*)src->pixels + srcoffset;
+		dstoffset = w*(dy + cur_h) + dx;
+		*srcpixels = (uint32_t*)dst->pixels + dstoffset;
+		for(int k = 0; k < w; k++){
+			*((uint32_t*)dstpixels+k) = *((uint32_t*)srcpixels+k);
+		}
+
+	}
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
