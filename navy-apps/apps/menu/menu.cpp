e9b182311d4cc2cee8317ea60e9266d572764fcb
tracer-ics2021 <tracer@njuics.org>
2021-11-30 23:58:01 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 1d999f810535485e90541e66f4a573e1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:58:01 up  4:03,  1 user,  load average: 0.35, 0.34, 0.23

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -96,8 +96,8 @@ int main(int argc, char *argv[], char *envp[]) {
       case SDLK_7: i = 7; break;
       case SDLK_8: i = 8; break;
       case SDLK_9: i = 9; break;
-      case SDLK_LEFT: prev(); break;
-      case SDLK_RIGHT: next(); break;
+      case SDLK_LEFT: printf("\nprev\n"); prev(); break;
+      case SDLK_RIGHT: printf("\nnext\n"); next(); break;
     }
 
     if (i != -1 && i <= i_max) {
