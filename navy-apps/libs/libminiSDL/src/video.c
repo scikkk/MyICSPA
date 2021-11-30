da697ce384a0eb48cef437c5a3902c825191128b
tracer-ics2021 <tracer@njuics.org>
2021-11-30 23:54:01 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 1d999f810535485e90541e66f4a573e1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:54:01 up  3:59,  1 user,  load average: 0.09, 0.11, 0.14

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -24,8 +24,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 		h = srcrect->h;
 	}
 	if (dstrect == NULL){
-		dx = 0;
-		dy = 0;
+		x = 0;
+		y = 0;
 	}
 	else{
 	dx = dstrect->x;
@@ -34,9 +34,9 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	uint32_t *srcpixels, *dstpixels, srcoffset, dstoffset;
 	for(int cur_h = 0; cur_h < h; cur_h++){
 		printf("x=%d\ty=%d\n", dx, dy+cur_h);
-		srcoffset = w*(sy + cur_h) + sx;
+		srcoffset = (src->)*(sy + cur_h) + sx;
 		srcpixels = (uint32_t*)(src->pixels) + srcoffset;
-		dstoffset = w*(dy + cur_h) + dx;
+		dstoffset = (dst->w)*(dy + cur_h) + dx;
 		dstpixels = (uint32_t*)(dst->pixels) + dstoffset;
 		for(int k = 0; k < w; k++){
 			*((uint32_t*)dstpixels+k) = *((uint32_t*)srcpixels+k);
