db3ff364706578d59a4ed2c741e863bbd58d812b
tracer-ics2021 <tracer@njuics.org>
2021-12-01 21:34:51 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 3ddd88096b1f41c49354a7167a08042e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:34:51 up  2:38,  1 user,  load average: 0.45, 0.22, 0.22

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -78,7 +78,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 	int bytes_num = dst->format->BytesPerPixel; 
-	assert(bytes_num == 8 || bytes_num == 8);
+	assert(bytes_num == 4);
 	int x,y,w,h,offset=0;
 	if(dstrect == NULL){
 		x = 0;
