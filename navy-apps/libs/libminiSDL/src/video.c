b7757bd1dfbcda94bd820ce91d72795cb5403e1f
tracer-ics2021 <tracer@njuics.org>
2021-12-01 22:27:16 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 3ddd88096b1f41c49354a7167a08042e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  22:27:16 up  3:30,  1 user,  load average: 0.22, 0.25, 0.21

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -128,11 +128,8 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 		for(int k = 0; k < size; k++){
 			assert(pixels_8[k] < ncolors);
 			pixels[k] = colors[pixels_8[k]].val;
-			printf("k=%d:%p\n", k, pixels[k]);
-			/* pixels[k] = 0xff000000; */ 
-			/* pixels[k] = 0x00ff0000; */ 
-			/* pixels[k] = 0x0000ff00; */ 
-			/* pixels[k] = 0x000000ff; */ 
+			/* printf("k=%d:%p\n", k, pixels[k]); */
+			pixels[k] = 0x848484; 
 		}
 	}
 
