a3d63af309020368d9aeea917764e6c6a7e180da
tracer-ics2021 <tracer@njuics.org>
2021-12-01 21:04:47 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 3ddd88096b1f41c49354a7167a08042e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:04:47 up  2:08,  1 user,  load average: 0.24, 0.18, 0.22

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -39,21 +39,43 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	assert(dx >= 0 && dy >= 0);
 	assert(src->w && dst->w);
 
+	if (bytes_num == 4){
+
+	/* printf("w=%d\th=%d\tsw=%d\tsh=%d\tdw=%d\tdh=%d\n", w, h, src->w, src->h, dst->w, dst->h); */
+	uint32_t *srcpixels, *dstpixels, srcoffset, dstoffset;
+	for(int cur_h = 0; cur_h < h; cur_h++){
+		/* printf("sx=%d\tsy=%d\n", sx, sy+cur_h); */
+		/* printf("dx=%d\tdy=%d\n", dx, dy+cur_h); */
+		srcoffset = (src->w)*(sy + cur_h) + sx;
+		srcpixels = (uint32_t*)src->pixels + srcoffset;
+		dstoffset = (dst->w)*(dy + cur_h) + dx;
+		dstpixels = (uint32_t*)dst->pixels + dstoffset;
+		/* printf("dx=%d,dy=%d\n", dx, dy+cur_h); */
+		/* printf("sx=%d,sy=%d\n", sx, sy+cur_h); */
+		/* printf("soff:%d\tdoff:%d\n", srcoffset, dstoffset); */
+		for(int k = 0; k < w; k++){
+			dstpixels[k] = srcpixels[k];
+		}
+	}
+	}
+	else{
+	
 	/* printf("w=%d\th=%d\tsw=%d\tsh=%d\tdw=%d\tdh=%d\n", w, h, src->w, src->h, dst->w, dst->h); */
 	uint8_t *srcpixels, *dstpixels, srcoffset, dstoffset;
 	for(int cur_h = 0; cur_h < h; cur_h++){
 		/* printf("sx=%d\tsy=%d\n", sx, sy+cur_h); */
 		/* printf("dx=%d\tdy=%d\n", dx, dy+cur_h); */
 		srcoffset = (src->w)*(sy + cur_h) + sx;
-		srcpixels = (uint8_t*)src->pixels + srcoffset*bytes_num;
+		srcpixels = (uint8_t*)src->pixels + srcoffset;
 		dstoffset = (dst->w)*(dy + cur_h) + dx;
-		dstpixels = (uint8_t*)dst->pixels + dstoffset*bytes_num;
+		dstpixels = (uint8_t*)dst->pixels + dstoffset;
 		/* printf("dx=%d,dy=%d\n", dx, dy+cur_h); */
 		/* printf("sx=%d,sy=%d\n", sx, sy+cur_h); */
 		/* printf("soff:%d\tdoff:%d\n", srcoffset, dstoffset); */
-		for(int k = 0; k < w*bytes_num; k++){
+		for(int k = 0; k < w; k++){
 			dstpixels[k] = srcpixels[k];
 		}
+	}
 	}
 }
 
