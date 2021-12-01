a7f33f13eb6f6b45ce4f546762ea7e226530562f
tracer-ics2021 <tracer@njuics.org>
2021-12-01 17:24:33 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 99c3f5b2c87d4c55baa7c166882f77ea     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:24:33 up  1:09,  1 user,  load average: 0.02, 0.05, 0.07

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -29,6 +29,7 @@ int SDL_PollEvent(SDL_Event *event) {
 			printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
 			return 0;
 		}
+
 		if(!strcmp(name, "0")){
 
 			event->key.keysym.sym = SDLK_0;
@@ -60,8 +61,114 @@ int SDL_PollEvent(SDL_Event *event) {
 		else if(!strcmp(name, "9")){
 			event->key.keysym.sym = SDLK_9; 
 		}
+		else if(!strcmp(name, "A")){
+			event->key.keysym.sym = SDLK_A; 
+		}
+
+		else if(!strcmp(name, "B")){
+			event->key.keysym.sym = SDLK_B; 
+		}
+		else if(!strcmp(name, "C")){
+			event->key.keysym.sym = SDLK_C; 
+		}
+		else if(!strcmp(name, "D")){
+			event->key.keysym.sym = SDLK_D; 
+		}
+		else if(!strcmp(name, "E")){
+			event->key.keysym.sym = SDLK_E; 
+		}
+		else if(!strcmp(name, "F")){
+			event->key.keysym.sym = SDLK_F; 
+		}
+		else if(!strcmp(name, "G")){
+			event->key.keysym.sym = SDLK_G; 
+		}
+		else if(!strcmp(name, "H")){
+			event->key.keysym.sym = SDLK_H; 
+		}
+		else if(!strcmp(name, "I")){
+			event->key.keysym.sym = SDLK_I; 
+		}
 		else if(!strcmp(name, "J")){
 			event->key.keysym.sym = SDLK_J; 
+		}
+		else if(!strcmp(name, "K")){
+			event->key.keysym.sym = SDLK_K; 
+		}
+		else if(!strcmp(name, "L")){
+			event->key.keysym.sym = SDLK_L; 
+		}
+		else if(!strcmp(name, "M")){
+			event->key.keysym.sym = SDLK_M; 
+		}
+		else if(!strcmp(name, "N")){
+			event->key.keysym.sym = SDLK_N; 
+		}
+		else if(!strcmp(name, "O")){
+			event->key.keysym.sym = SDLK_O; 
+		}
+		else if(!strcmp(name, "P")){
+			event->key.keysym.sym = SDLK_P; 
+		}
+		else if(!strcmp(name, "Q")){
+			event->key.keysym.sym = SDLK_Q; 
+		}
+		else if(!strcmp(name, "R")){
+			event->key.keysym.sym = SDLK_R; 
+		}
+		else if(!strcmp(name, "S")){
+			event->key.keysym.sym = SDLK_S; 
+		}
+		else if(!strcmp(name, "T")){
+			event->key.keysym.sym = SDLK_T; 
+		}
+		else if(!strcmp(name, "U")){
+			event->key.keysym.sym = SDLK_U; 
+		}
+		else if(!strcmp(name, "V")){
+			event->key.keysym.sym = SDLK_V; 
+		}
+		else if(!strcmp(name, "W")){
+			event->key.keysym.sym = SDLK_W; 
+		}
+		else if(!strcmp(name, "X")){
+			event->key.keysym.sym = SDLK_X; 
+		}
+		else if(!strcmp(name, "Y")){
+			event->key.keysym.sym = SDLK_Y; 
+		}
+		else if(!strcmp(name, "Z")){
+			event->key.keysym.sym = SDLK_Z; 
+		}
+
+		else if(!strcmp(name, "LEFTBRACKET")){
+			event->key.keysym.sym = SDLK_LEFTBRACKET; 
+		}
+		else if(!strcmp(name, "RIGHTBRACKET")){
+			event->key.keysym.sym = SDLK_RIGHTBRACKET; 
+		}
+		else if(!strcmp(name, "COMMA")){
+			event->key.keysym.sym = SDLK_COMMA; 
+		}
+		else if(!strcmp(name, "PERIOD")){
+			event->key.keysym.sym = SDLK_PERIOD; 
+		}
+
+		else if(!strcmp(name, "SEMICOLON")){
+			event->key.keysym.sym = SDLK_SEMICOLON; 
+		}
+		else if(!strcmp(name, "APOSTROPHE")){
+			event->key.keysym.sym = SDLK_APOSTROPHE; 
+		}
+		else if(!strcmp(name, "SPACE")){
+			event->key.keysym.sym = SDLK_SPACE; 
+		}
+
+		else if(!strcmp(name, "SLASH")){
+			event->key.keysym.sym = SDLK_SLASH; 
+		}
+		else if(!strcmp(name, "BACKSLASH")){
+			event->key.keysym.sym = SDLK_BACKSLASH; 
 		}
 		else if(!strcmp(name, "UP")){
 			event->key.keysym.sym = SDLK_UP; 
@@ -75,12 +182,108 @@ int SDL_PollEvent(SDL_Event *event) {
 		else if(!strcmp(name, "RIGHT")){
 			event->key.keysym.sym = SDLK_RIGHT; 
 		}
-		else if(!strcmp(name, "K")){
-			event->key.keysym.sym = SDLK_K; 
+		else if(!strcmp(name, "BACKSPACE")){
+			event->key.keysym.sym = SDLK_BACKSPACE; 
+		}
+		else if(!strcmp(name, "DELETE")){
+			event->key.keysym.sym = SDLK_DELETE; 
 		}
 
-		else if(!strcmp(name, "G")){
-			event->key.keysym.sym = SDLK_G; 
+		else if(!strcmp(name, "RETURN")){
+			event->key.keysym.sym = SDLK_RETURN; 
+		}
+		else if(!strcmp(name, "MINUS")){
+			event->key.keysym.sym = SDLK_MINUS; 
+		}
+		else if(!strcmp(name, "EQUALS")){
+			event->key.keysym.sym = SDLK_EQUALS; 
+		}
+
+		else if(!strcmp(name, "INSERT")){
+			event->key.keysym.sym = SDLK_INSERT; 
+		}
+		else if(!strcmp(name, "HOME")){
+			event->key.keysym.sym = SDLK_HOME; 
+		}
+		else if(!strcmp(name, "END")){
+			event->key.keysym.sym = SDLK_END; 
+		}
+
+		else if(!strcmp(name, "PAGEUP")){
+			event->key.keysym.sym = SDLK_PAGEUP; 
+		}
+		else if(!strcmp(name, "PAGEDOWN")){
+			event->key.keysym.sym = SDLK_PAGEDOWN; 
+		}
+		else if(!strcmp(name, "TAB")){
+			event->key.keysym.sym = SDLK_TAB; 
+		}
+		else if(!strcmp(name, "F1")){
+			event->key.keysym.sym = SDLK_F1; 
+		}
+		else if(!strcmp(name, "F2")){
+			event->key.keysym.sym = SDLK_F2; 
+		}
+		else if(!strcmp(name, "F3")){
+			event->key.keysym.sym = SDLK_F3; 
+		}
+		else if(!strcmp(name, "F4")){
+			event->key.keysym.sym = SDLK_F4; 
+		}
+		else if(!strcmp(name, "F5")){
+			event->key.keysym.sym = SDLK_F5; 
+		}
+		else if(!strcmp(name, "F6")){
+			event->key.keysym.sym = SDLK_F6; 
+		}
+		else if(!strcmp(name, "F7")){
+			event->key.keysym.sym = SDLK_F7; 
+		}
+		else if(!strcmp(name, "F8")){
+			event->key.keysym.sym = SDLK_F8; 
+		}
+		else if(!strcmp(name, "F9")){
+			event->key.keysym.sym = SDLK_F9; 
+		}
+		else if(!strcmp(name, "F10")){
+			event->key.keysym.sym = SDLK_F10; 
+		}
+		else if(!strcmp(name, "F11")){
+			event->key.keysym.sym = SDLK_F11; 
+		}
+		else if(!strcmp(name, "F12")){
+			event->key.keysym.sym = SDLK_F12; 
+		}
+		else if(!strcmp(name, "ESCAPE")){
+			event->key.keysym.sym = SDLK_ESCAPE; 
+		}
+		else if(!strcmp(name, "GRAVE")){
+			event->key.keysym.sym = SDLK_GRAVE; 
+		}
+
+		else if(!strcmp(name, "CAPSLOCK")){
+			event->key.keysym.sym = SDLK_CAPSLOCK; 
+		}
+		else if(!strcmp(name, "LSHIFT")){
+			event->key.keysym.sym = SDLK_LSHIFT; 
+		}
+		else if(!strcmp(name, "RSHIFT")){
+			event->key.keysym.sym = SDLK_RSHIFT; 
+		}
+		else if(!strcmp(name, "LCTR")){
+			event->key.keysym.sym = SDLK_LCTR; 
+		}
+		else if(!strcmp(name, "RCTR")){
+			event->key.keysym.sym = SDLK_RCTR; 
+		}
+		else if(!strcmp(name, "LALT")){
+			event->key.keysym.sym = SDLK_LALT; 
+		}
+		else if(!strcmp(name, "RALT")){
+			event->key.keysym.sym = SDLK_RALT; 
+		}
+		else if(!strcmp(name, "APPLICATION")){
+			event->key.keysym.sym = SDLK_APPLICATION; 
 		}
 		else{
 			printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
