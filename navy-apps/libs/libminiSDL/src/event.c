e92b033ecbaeeb489fc2d2aec9eed30094c54566
tracer-ics2021 <tracer@njuics.org>
2021-12-01 16:53:58 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 99c3f5b2c87d4c55baa7c166882f77ea     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  16:53:58 up 38 min,  1 user,  load average: 0.02, 0.15, 0.19

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -16,171 +16,90 @@ int SDL_PushEvent(SDL_Event *ev) {
 int SDL_PollEvent(SDL_Event *event) {
 	printf("\n\nevent.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
 
-		char buf[64];
-		if (NDL_PollEvent(buf, sizeof(buf))) {
-			/* printf("receive event: %s\n", buf); */
-			char type[3], name[20];
-			sscanf(buf, "%s %s\n", type, name);
-			if(strcmp(type, "ku") == 0){
-				event->type = SDL_KEYUP;
-			}
-			else if(strcmp(type, "kd") == 0){
-				event->type = SDL_KEYDOWN;
-			}
-			else{
-				printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
-				return 0;
-			}
-			if(!strcmp(name, "0")){
-
-				event->key.keysym.sym = SDLK_0;
-			}
-			else if(!strcmp(name, "1")){
-				event->key.keysym.sym = SDLK_1; 
-			}
-			else if(!strcmp(name, "2")){
-				event->key.keysym.sym = SDLK_2; 
-			}
-			else if(!strcmp(name, "3")){
-				event->key.keysym.sym = SDLK_3; 
-			}
-			else if(!strcmp(name, "4")){
-				event->key.keysym.sym = SDLK_4; 
-			}
-			else if(!strcmp(name, "5")){
-				event->key.keysym.sym = SDLK_5; 
-			}
-			else if(!strcmp(name, "6")){
-				event->key.keysym.sym = SDLK_6;
-			}
-			else if(!strcmp(name, "7")){
-				event->key.keysym.sym = SDLK_7; 
-			}
-			else if(!strcmp(name, "8")){
-				event->key.keysym.sym = SDLK_8;
-			}
-			else if(!strcmp(name, "9")){
-				event->key.keysym.sym = SDLK_9; 
-			}
-			else if(!strcmp(name, "J")){
-				event->key.keysym.sym = SDLK_J; 
-			}
-			else if(!strcmp(name, "UP")){
-				event->key.keysym.sym = SDLK_UP; 
-			}
-			else if(!strcmp(name, "DOWN")){
-				event->key.keysym.sym = SDLK_DOWN; 
-			}
-			else if(!strcmp(name, "LEFT")){
-				event->key.keysym.sym = SDLK_LEFT; 
-			}
-			else if(!strcmp(name, "RIGHT")){
-				event->key.keysym.sym = SDLK_RIGHT; 
-			}
-			else if(!strcmp(name, "K")){
-				event->key.keysym.sym = SDLK_K; 
-			}
-
-			else if(!strcmp(name, "G")){
-				event->key.keysym.sym = SDLK_G; 
-			}
-			else{
-				printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
-				return 0;
-			}
-			/* printf("%s\n", type); */
-			/* printf("%s\n", name); */
-			/* printf("%d\n", __LINE__); */
-
-			return 1;
+	char buf[64];
+	if (NDL_PollEvent(buf, sizeof(buf))) {
+		/* printf("receive event: %s\n", buf); */
+		char type[3], name[20];
+		sscanf(buf, "%s %s\n", type, name);
+		if(strcmp(type, "ku") == 0){
+			event->type = SDL_KEYUP;
 		}
+		else if(strcmp(type, "kd") == 0){
+			event->type = SDL_KEYDOWN;
+		}
+		else{
+			printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
+			return 0;
+		}
+		if(!strcmp(name, "0")){
+
+			event->key.keysym.sym = SDLK_0;
+		}
+		else if(!strcmp(name, "1")){
+			event->key.keysym.sym = SDLK_1; 
+		}
+		else if(!strcmp(name, "2")){
+			event->key.keysym.sym = SDLK_2; 
+		}
+		else if(!strcmp(name, "3")){
+			event->key.keysym.sym = SDLK_3; 
+		}
+		else if(!strcmp(name, "4")){
+			event->key.keysym.sym = SDLK_4; 
+		}
+		else if(!strcmp(name, "5")){
+			event->key.keysym.sym = SDLK_5; 
+		}
+		else if(!strcmp(name, "6")){
+			event->key.keysym.sym = SDLK_6;
+		}
+		else if(!strcmp(name, "7")){
+			event->key.keysym.sym = SDLK_7; 
+		}
+		else if(!strcmp(name, "8")){
+			event->key.keysym.sym = SDLK_8;
+		}
+		else if(!strcmp(name, "9")){
+			event->key.keysym.sym = SDLK_9; 
+		}
+		else if(!strcmp(name, "J")){
+			event->key.keysym.sym = SDLK_J; 
+		}
+		else if(!strcmp(name, "UP")){
+			event->key.keysym.sym = SDLK_UP; 
+		}
+		else if(!strcmp(name, "DOWN")){
+			event->key.keysym.sym = SDLK_DOWN; 
+		}
+		else if(!strcmp(name, "LEFT")){
+			event->key.keysym.sym = SDLK_LEFT; 
+		}
+		else if(!strcmp(name, "RIGHT")){
+			event->key.keysym.sym = SDLK_RIGHT; 
+		}
+		else if(!strcmp(name, "K")){
+			event->key.keysym.sym = SDLK_K; 
+		}
+
+		else if(!strcmp(name, "G")){
+			event->key.keysym.sym = SDLK_G; 
+		}
+		else{
+			printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
+			return 0;
+		}
+		/* printf("%s\n", type); */
+		/* printf("%s\n", name); */
+		/* printf("%d\n", __LINE__); */
+
+		return 1;
+	}
 	return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-	printf("\n\nevent.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
-	while (1) {
-		char buf[64];
-		if (NDL_PollEvent(buf, sizeof(buf))) {
-			/* printf("receive event: %s\n", buf); */
-			char type[3], name[20];
-			sscanf(buf, "%s %s\n", type, name);
-			if(strcmp(type, "ku") == 0){
-				event->type = SDL_KEYUP;
-			}
-			else if(strcmp(type, "kd") == 0){
-				event->type = SDL_KEYDOWN;
-			}
-			else{
-				printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
-				return 0;
-			}
-			if(!strcmp(name, "0")){
-
-				event->key.keysym.sym = SDLK_0;
-			}
-			else if(!strcmp(name, "1")){
-				event->key.keysym.sym = SDLK_1; 
-			}
-			else if(!strcmp(name, "2")){
-				event->key.keysym.sym = SDLK_2; 
-			}
-			else if(!strcmp(name, "3")){
-				event->key.keysym.sym = SDLK_3; 
-			}
-			else if(!strcmp(name, "4")){
-				event->key.keysym.sym = SDLK_4; 
-			}
-			else if(!strcmp(name, "5")){
-				event->key.keysym.sym = SDLK_5; 
-			}
-			else if(!strcmp(name, "6")){
-				event->key.keysym.sym = SDLK_6;
-			}
-			else if(!strcmp(name, "7")){
-				event->key.keysym.sym = SDLK_7; 
-			}
-			else if(!strcmp(name, "8")){
-				event->key.keysym.sym = SDLK_8;
-			}
-			else if(!strcmp(name, "9")){
-				event->key.keysym.sym = SDLK_9; 
-			}
-			else if(!strcmp(name, "J")){
-				event->key.keysym.sym = SDLK_J; 
-			}
-			else if(!strcmp(name, "UP")){
-				event->key.keysym.sym = SDLK_UP; 
-			}
-			else if(!strcmp(name, "DOWN")){
-				event->key.keysym.sym = SDLK_DOWN; 
-			}
-			else if(!strcmp(name, "LEFT")){
-				event->key.keysym.sym = SDLK_LEFT; 
-			}
-			else if(!strcmp(name, "RIGHT")){
-				event->key.keysym.sym = SDLK_RIGHT; 
-			}
-			else if(!strcmp(name, "K")){
-				event->key.keysym.sym = SDLK_K; 
-			}
-
-			else if(!strcmp(name, "G")){
-				event->key.keysym.sym = SDLK_G; 
-			}
-			else{
-				printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
-				return 0;
-			}
-			/* printf("%s\n", type); */
-			/* printf("%s\n", name); */
-			/* printf("%d\n", __LINE__); */
-
-			return 1;
-		}
-	}
-	printf("\n\n!!!!!!!!!Error:event.c:%d\n\n", __LINE__);
-	return 0;
+	while (!SDL_PollEvent(event));
+	return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
