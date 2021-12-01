15e2e634d3c51ad34cd69346b38d4a5751af6256
tracer-ics2021 <tracer@njuics.org>
2021-12-01 22:16:56 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 3ddd88096b1f41c49354a7167a08042e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  22:16:56 up  3:20,  1 user,  load average: 0.39, 0.28, 0.24

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -310,6 +310,7 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
 	printf("\n\nevent.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	while(1);
 	exit(-1);
 	return NULL;
 }
