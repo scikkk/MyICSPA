1ed0433586c0939d7a8bd3a2aca5227cd3cfb57c
tracer-ics2021 <tracer@njuics.org>
2021-12-01 23:25:44 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 3ddd88096b1f41c49354a7167a08042e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:25:44 up  4:29,  1 user,  load average: 0.33, 0.27, 0.19

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -123,10 +123,10 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 		uint8_t *pixels_8 = s->pixels;
 		SDL_Color *colors = s->format->palette->colors; 
 		int ncolors = s->format->palette->ncolors;
-		for(int k = 0; k < ncolors; k++){
-			SDL_Color *curcolor = &colors[k];
-			printf("%p==%p\n", curcolor->val, curcolor->a<<24 | curcolor->r<<16 | curcolor->g<<8 | curcolor->b);
-		}
+		/* for(int k = 0; k < ncolors; k++){ */
+		/* 	SDL_Color *curcolor = &colors[k]; */
+		/* 	printf("%p==%p\n", curcolor->val, curcolor->a<<24 | curcolor->r<<16 | curcolor->g<<8 | curcolor->b); */
+		/* } */
 		int size = s->w*s->h;
 		pixels = (uint32_t*)malloc(size*4);
 		for(int k = 0; k < size; k++){
