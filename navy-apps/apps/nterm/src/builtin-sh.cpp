6467c4f32ca7698457562a22344ec59a99eaf92f
tracer-ics2021 <tracer@njuics.org>
2021-12-04 19:52:53 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 81fb9f2fb4e041128a919ccdba3cbfea     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:52:53 up  1:18,  1 user,  load average: 0.29, 0.21, 0.24

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -24,7 +24,7 @@ static void sh_prompt() {
 
 static void sh_handle_cmd(const char *cmd) {
 	if(cmd[0] == '/'){
-		execve(cmd, NULL, NULL);	
+		execvp(cmd, NULL);	
 	}
 	if (cmd[0] == 'e' && cmd[1] == 'c' && cmd[2] == 'h' && cmd[3] == 'o'){
 		sh_printf("%s", &cmd[4]);
@@ -34,6 +34,7 @@ static void sh_handle_cmd(const char *cmd) {
 void builtin_sh_run() {
   sh_banner();
   sh_prompt();
+  setenv("PATH", "/bin", 0);
 
   while (1) {
     SDL_Event ev;
