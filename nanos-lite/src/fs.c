96dbc7199304c8e44b234430b6c0856fd73627b7
tracer-ics2021 <tracer@njuics.org>
2021-12-04 19:57:33 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 81fb9f2fb4e041128a919ccdba3cbfea     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:57:33 up  1:23,  1 user,  load average: 0.09, 0.23, 0.25

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,13 +62,13 @@ size_t get_ramdisk_size();
 #define FILE_NUM 128
 
 int fs_open(const char *pathname, int flags, int mode){
-if(strcmp("/dev/events", pathname))	printf("fs.c:open %s\n", pathname);
+	if(strcmp("/dev/events", pathname)&&strcmp("/dev/fb", pathname)) printf("fs.c:open %s\n", pathname);
 	int ret = -1;
 	while(ret < FILE_NUM && strcmp(file_table[++ret].name, pathname) != 0);
 	file_table[ret].open_offset = 0;
 	if(ret >= FD_NORMAL){
 		file_table[ret].read = ramdisk_read;
- 		file_table[ret].write = ramdisk_write;
+		file_table[ret].write = ramdisk_write;
 	}
 	assert(ret < FILE_NUM);
 	/* Log("fd: %d\n", ret); */
