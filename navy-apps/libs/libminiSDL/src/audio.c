0ba986f0d94a4ee52e0d25f0b5ad5bb2ce7b54cc
tracer-ics2021 <tracer@njuics.org>
2021-12-05 17:15:46 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 4167adf84146483382862cbb27443326     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:15:46 up  1:13,  1 user,  load average: 0.24, 0.32, 0.53

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -2,35 +2,35 @@
 #include <SDL.h>
 
 int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
-  return 0;
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
+	return 0;
 }
 
 void SDL_CloseAudio() {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
 }
 
 void SDL_PauseAudio(int pause_on) {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
 }
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
-  return NULL;
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
+	return NULL;
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
 }
 
 void SDL_LockAudio() {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
 }
 
 void SDL_UnlockAudio() {
-	printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__);
+	/* printf("\n\naudio.c: %d: TODO!!!!!!!!!!!!!!!!!!!\n\n", __LINE__); */
 }
