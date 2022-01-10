6d975a923703e07f18c89ea2765c22d585724ea4
tracer-ics2021 <tracer@njuics.org>
2022-01-10 22:45:14 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 3bfccc20a2c64d2787045afb1e45121a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  22:45:15 up  1:32,  1 user,  load average: 0.72, 0.30, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -54,18 +54,18 @@ void init_proc() {
 Context* schedule(Context *prev) {
 	// wk 4.1
 	// save the context pointer
-	printf("prev=%p\n", prev);
+	/* printf("prev=%p\n", prev); */
 	current->cp = prev;
 	// always select pcb[0] as the new process
 	current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 	// then return the new context
-	printf("cur-cp=%p\n", current->cp);
+	/* printf("cur-cp=%p\n", current->cp); */
 
-	printf("pcb0->cp=%p\n", pcb[0].cp);
-	printf("pcb1->cp=%p\n", pcb[1].cp);
+	/* printf("pcb0->cp=%p\n", pcb[0].cp); */
+	/* printf("pcb1->cp=%p\n", pcb[1].cp); */
 
-	printf("mepc=%p\n", (current->cp)->mepc);
-	printf("gpr1=%p\n", (current->cp)->GPR1);
+	/* printf("mepc=%p\n", (current->cp)->mepc); */
+	/* printf("gpr1=%p\n", (current->cp)->GPR1); */
 	return current->cp;
 	// wk 4.1
 	/* return NULL; */
