0d43f829c7166390a007cdb6b8eb1c81e93ae892
tracer-ics2021 <tracer@njuics.org>
2022-01-11 19:31:34 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: b23d867830de474baecd24b1d51b09f3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:31:34 up 18 min,  1 user,  load average: 0.18, 0.26, 0.26

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -54,9 +54,9 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
 	/* panic("Not implemented"); */
-	if(s1 == NULL || s2 == NULL){
-		printf("s1=%s\ts2=%s\n", s1, s2);
-	}
+	/* if(s1 == NULL || s2 == NULL){ */
+	/* 	printf("s1=%s\ts2=%s\n", s1, s2); */
+	/* } */
 	assert((s1 != NULL) && (s2 != NULL));
 	while((*s1 != '\0') && (*s1 == *s2)){
 		s1++;
