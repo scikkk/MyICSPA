777f557c7df56021e2fe991f681a3caeb8697e1c
tracer-ics2021 <tracer@njuics.org>
2022-01-11 22:15:50 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: f77ed044964543089e32969fba8a8d27     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  22:15:50 up  2:29,  1 user,  load average: 0.15, 0.10, 0.04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -91,13 +91,13 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
 
 	printf("context_uload:filename=%s\n", filename);
-	printf("argv=%p\tenvp=%p\n", argv, envp[0]);
+	printf("argv=%p\tenvp=%p\n", argv, envp);
 	Area kstack = {pcb, pcb+1};
 	uintptr_t entry = loader(pcb, filename);
 	pcb->cp = ucontext(NULL, kstack, (void*)entry);
 	pcb->cp->GPRx = (uintptr_t)new_page(8);
 	int argc = 0, envpc = 0;
-	/* if(envp)	printf("uload envp[0]=%p\n", envp[0]); */
+	if(envp)	printf("uload envp[0]=%p\n", envp[0]);
 	if(argv){
 		argc = -1;
 		while(argv[++argc]){
