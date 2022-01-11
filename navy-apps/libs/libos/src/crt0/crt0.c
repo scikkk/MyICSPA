f1b3fda5a9a6185e4741d2e876b93b8b2d5eec95
tracer-ics2021 <tracer@njuics.org>
2022-01-11 12:58:04 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 604581e87d8c42dc9cbbddecd5583be1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  12:58:04 up  1:28,  1 user,  load average: 0.42, 0.32, 0.18

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -2,24 +2,17 @@
 #include <stdlib.h>
 #include <assert.h>
 
-// wk
-#include <stdio.h>
-// wk
+/* // wk */
+/* #include <stdio.h> */
+/* // wk */
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-	printf("args=%p\n", args);
 	int argc = *((int*)args);
-	printf("call_main:argc=%d\n", argc);
 	char **argv = (char**)((uintptr_t)args+sizeof(int));
 	char **envp = (char**)(argv + argc + 1);
-	printf("argv=%p\tenvp=%p\n", argv, envp);
 	environ =  envp;
-	// wk
-	printf("chsnkmalvmcshjhbjnmcall_main=%s\n", argv[0]);
-	assert(0);
-	// wk
 	exit(main(argc, argv,  envp));
 	assert(0);
 }
