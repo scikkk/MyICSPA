9eb567d0562c011e7a624836fcab4270f3b39fb2
tracer-ics2021 <tracer@njuics.org>
2022-01-11 17:10:51 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: bf6249d4676b474cb04a6229475e9d27     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  17:10:51 up  1:10,  1 user,  load average: 0.64, 0.43, 0.27

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,15 +27,15 @@ void init_proc() {
 	// wk 4.1
 	context_kload(&pcb[0], hello_fun, (void*)0x99999999);
 
-	char *argv[] = {"--skip", NULL};
-	context_uload(&pcb[1], "/bin/pal", argv, NULL);
+	/* char *argv[] = {"--skip", NULL}; */
+	/* context_uload(&pcb[1], "/bin/pal", argv, NULL); */
 
 	/* char *argv[] = {"/bin/exec-test", NULL}; */
 	/* context_uload(&pcb[1], "/bin/exec-test", argv, NULL); */
 	
 	/* context_uload(&pcb[1], "/bin/menu", NULL, NULL); */
 
-	/* context_uload(&pcb[1], "/bin/nterm", NULL, NULL); */
+	context_uload(&pcb[1], "/bin/nterm", NULL, NULL);
 
 	switch_boot_pcb();
 	// wk 4.1
