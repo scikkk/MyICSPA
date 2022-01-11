e63abfcd0111555c6d7a1ba15404a277cd8356ec
tracer-ics2021 <tracer@njuics.org>
2022-01-11 14:38:16 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 604581e87d8c42dc9cbbddecd5583be1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  14:38:16 up  3:08,  1 user,  load average: 0.18, 0.08, 0.03

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,12 +24,11 @@ extern void context_kload(PCB *pcb, void (*entry)(void *), void *arg);
 extern void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 void init_proc() {
 	// wk 4.1
-	/* context_kload(&pcb[0], hello_fun, (void*)0x99); */
+	context_kload(&pcb[0], hello_fun, (void*)0x99);
 	/* context_kload(&pcb[1], hello_fun, (void*)0x66); */
-	/* context_uload(&pcb[0], "/bin/hello"); */
-	/* char *argv[] = {"--skip", NULL}; */
-	/* context_uload(&pcb[1], "/bin/pal", argv, NULL); */
-	context_uload(&pcb[0], "/bin/exec-test", NULL, NULL);
+	char *argv[] = {"--skip", NULL};
+	context_uload(&pcb[1], "/bin/pal", argv, NULL);
+	/* context_uload(&pcb[0], "/bin/exec-test", NULL, NULL); */
 	switch_boot_pcb();
 	// wk 4.1
 
