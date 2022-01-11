0e0334dbc4e973f0a7cc9d6c220ff28bd2ed0c0c
tracer-ics2021 <tracer@njuics.org>
2022-01-11 17:10:28 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: bf6249d4676b474cb04a6229475e9d27     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  17:10:28 up  1:10,  1 user,  load average: 0.63, 0.41, 0.26

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,7 +13,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
 	int j = 1;
 	while (1) {
-		if(j % 999 == 0)
+		if(j % 512 == 0)
 			Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
 		j ++;
 		yield();
