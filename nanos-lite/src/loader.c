bb1834cef7900859f22156275be804cc98fe46e9
tracer-ics2021 <tracer@njuics.org>
2022-01-11 12:53:00 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 604581e87d8c42dc9cbbddecd5583be1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  12:53:00 up  1:23,  1 user,  load average: 0.34, 0.20, 0.12

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -124,7 +124,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 	memset((void*)envp_start + 4*argc, 0, 4);
 	printf("\nend of context_uload\n");
 	printf("argc=%d\tenvpc=%d\n", argc, envpc);
-	printf("argv[0]=%s\n", argv[0]);
+	printf("ffoo10=%s\n", (char*)0x87ff0010);
 	printf("argv[0]=%p\n", argv[0]);
 }
 // wk 4.1
