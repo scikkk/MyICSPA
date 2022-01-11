7f81b0e64f4a48b1ca57214d4c1ece46a8bf0ea1
tracer-ics2021 <tracer@njuics.org>
2022-01-11 15:59:20 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 604581e87d8c42dc9cbbddecd5583be1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  15:59:21 up  4:29,  1 user,  load average: 0.20, 0.22, 0.18

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -5,11 +5,11 @@
 
 int main(int argc, char *argv[]) {
 	printf("main argv=%p\n", argv);
-  int n = (argc >= 2 ? atoi(argv[1]) : 1);
-  printf("%s: argv[1] = %d\n", argv[0], n);
+	int n = (argc >= 2 ? atoi(argv[1]) : 1);
+	printf("wk%s: argv[1] = %d\n", argv[0], n);
 
-  char buf[16];
-  sprintf(buf, "%d", n + 1);
-  execl(argv[0], argv[0], buf, NULL);
-  return 0;
+	char buf[16];
+	sprintf(buf, "%d", n + 1);
+	execl(argv[0], argv[0], buf, NULL);
+	return 0;
 }
