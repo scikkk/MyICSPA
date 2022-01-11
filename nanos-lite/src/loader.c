d048459e72ab5be47c9e0996107b09e275c87987
tracer-ics2021 <tracer@njuics.org>
2022-01-11 22:25:13 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: f77ed044964543089e32969fba8a8d27     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  22:25:13 up  2:38,  1 user,  load average: 0.14, 0.28, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -94,6 +94,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 	printf("context_uload:filename=%s\n", filename);
 	printf("argv=%p\tenvp=%p\n", argv, envp);
 	if(envp)	printf("uload envp[0]=%p\n", envp[0]);
+	if(envp)	printf("uload envp[0]=%s\n", envp[0]);
 	Area kstack = {pcb, pcb+1};
 	uintptr_t entry = loader(pcb, filename);
 	pcb->cp = ucontext(NULL, kstack, (void*)entry);
