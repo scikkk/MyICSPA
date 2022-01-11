950027854fb5d36d68b55f746de2fd8014182858
tracer-ics2021 <tracer@njuics.org>
2022-01-11 22:22:58 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: f77ed044964543089e32969fba8a8d27     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  22:22:58 up  2:36,  1 user,  load average: 0.25, 0.32, 0.17

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -92,7 +92,7 @@ int sys_execve(const char *pathname, char *const argv[], char *const envp[]){
 		return -2;
 	}
 	printf("sys: argv=%p\tenvp[0]=%s\n", argv, envp[0]);
-	printf("sys: argv[0]=%p\tenvp[1]=%p\n", argv[0], envp[1]);
+	printf("sys: argv[0]=%p\tenvp[0]=%p\n", argv[0], envp[0]);
     context_uload(current, pathname, argv, envp);
 	switch_boot_pcb();
 	yield();
