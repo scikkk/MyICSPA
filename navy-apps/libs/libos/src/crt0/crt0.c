978b35633ce5532cf402154a5007fad58cc606bf
tracer-ics2021 <tracer@njuics.org>
2022-01-11 12:12:27 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 604581e87d8c42dc9cbbddecd5583be1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  12:12:27 up 42 min,  1 user,  load average: 0.31, 0.11, 0.09

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
+	printf("args=%p\n", args);
 	int argc = *((int*)args);
 	char **argv = (char**)(args+4);
 	char **envp = (char**)(args + 4 + argc*4 + 4);
