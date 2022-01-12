484939eec5eefc7a6fec67e85b6c078557525d06
tracer-ics2021 <tracer@njuics.org>
2022-01-12 14:08:53 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d41388a34b844a80bbb0addf2c91ffd5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  14:08:53 up  3:34,  1 user,  load average: 0.13, 0.12, 0.10

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -36,4 +36,5 @@ static Context* do_event(Event e, Context* c) {
 void init_irq(void) {
 	Log("Initializing interrupt/exception handler...");
 	cte_init(do_event);
+	Log("Initializing interrupt/exception handler...");
 }
