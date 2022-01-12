19fd9420b0708c19cbb8bb721393c7c6ccc4086b
tracer-ics2021 <tracer@njuics.org>
2022-01-12 15:43:53 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d41388a34b844a80bbb0addf2c91ffd5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  15:43:53 up  5:09,  1 user,  load average: 0.13, 0.19, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -79,7 +79,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 				}
 			}
 			/* fs_read(fd, (void*)ph.p_vaddr, ph.p_filesz); */
-			printf("line:%dloader: vaddr=%p\n", __LINE__, ph.p_vaddr);
+			printf("line%d:loader: vaddr=%p\n", __LINE__, ph.p_vaddr);
 		} 
 	}
 	return header.e_entry;
