36fa0bdb6909df6b01601a6d6b7c8a705f02bc54
tracer-ics2021 <tracer@njuics.org>
2022-01-12 12:47:20 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d41388a34b844a80bbb0addf2c91ffd5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  12:47:20 up  2:28,  1 user,  load average: 0.16, 0.17, 0.16

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -8,62 +8,62 @@ static void (*pgfree_usr)(void*) = NULL;
 static int vme_enable = 0;
 
 static Area segments[] = {      // Kernel memory mappings
-  NEMU_PADDR_SPACE
+	NEMU_PADDR_SPACE
 };
 
 #define USER_SPACE RANGE(0x40000000, 0x80000000)
 
 static inline void set_satp(void *pdir) {
-  uintptr_t mode = 1ul << (__riscv_xlen - 1);
-  asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
+	uintptr_t mode = 1ul << (__riscv_xlen - 1);
+	asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
 }
 
 static inline uintptr_t get_satp() {
-  uintptr_t satp;
-  asm volatile("csrr %0, satp" : "=r"(satp));
-  return satp << 12;
+	uintptr_t satp;
+	asm volatile("csrr %0, satp" : "=r"(satp));
+	return satp << 12;
 }
 
 bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
-  pgalloc_usr = pgalloc_f;
-  pgfree_usr = pgfree_f;
+	pgalloc_usr = pgalloc_f;
+	pgfree_usr = pgfree_f;
 
-  kas.ptr = pgalloc_f(PGSIZE);
+	kas.ptr = pgalloc_f(PGSIZE);
 
-  int i;
-  for (i = 0; i < LENGTH(segments); i ++) {
-    void *va = segments[i].start;
-    for (; va < segments[i].end; va += PGSIZE) {
-      map(&kas, va, va, 0);
-    }
-  }
+	int i;
+	for (i = 0; i < LENGTH(segments); i ++) {
+		void *va = segments[i].start;
+		for (; va < segments[i].end; va += PGSIZE) {
+			map(&kas, va, va, 0);
+		}
+	}
 
-  set_satp(kas.ptr);
-  vme_enable = 1;
+	set_satp(kas.ptr);
+	vme_enable = 1;
 
-  return true;
+	return true;
 }
 
 void protect(AddrSpace *as) {
-  PTE *updir = (PTE*)(pgalloc_usr(PGSIZE));
-  as->ptr = updir;
-  as->area = USER_SPACE;
-  as->pgsize = PGSIZE;
-  // map kernel space
-  memcpy(updir, kas.ptr, PGSIZE);
+	PTE *updir = (PTE*)(pgalloc_usr(PGSIZE));
+	as->ptr = updir;
+	as->area = USER_SPACE;
+	as->pgsize = PGSIZE;
+	// map kernel space
+	memcpy(updir, kas.ptr, PGSIZE);
 }
 
 void unprotect(AddrSpace *as) {
 }
 
 void __am_get_cur_as(Context *c) {
-  c->pdir = (vme_enable ? (void *)get_satp() : NULL);
+	c->pdir = (vme_enable ? (void *)get_satp() : NULL);
 }
 
 void __am_switch(Context *c) {
-  if (vme_enable && c->pdir != NULL) {
-    set_satp(c->pdir);
-  }
+	if (vme_enable && c->pdir != NULL) {
+		set_satp(c->pdir);
+	}
 }
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
@@ -75,10 +75,13 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	/* pa = (pg_table[va >> 12] & ~0xfff) | (va & 0xfff); */
 	uintptr_t satp_ppn = pt1_base >> 12;
 	uintptr_t pte1_addr = (satp_ppn << 12) | (vpn1 << 2);
-	uintptr_t pte1 = (uintptr_t)pgalloc_usr(PGSIZE);
-	assert((pte1 & 0xfff) == 0);
-	pte1 |= 0x1;
-	*(uintptr_t*)pte1_addr = pte1; 
+	uintptr_t pte1 = *(uintptr_t*)pte1_addr;
+	if((pte1 & 0x1) == 0){
+		pte1 = (uintptr_t)pgalloc_usr(PGSIZE);
+		assert((pte1 & 0xfff) == 0);
+		pte1 |= 0x1;
+		*(uintptr_t*)pte1_addr = pte1; 
+	}
 	uintptr_t pte2_addr = (pte1 & ~0xfff) | (vpn0 << 2);
 	printf("pte2_addr=%p\n", pte2_addr);
 	*(uintptr_t*)pte2_addr = ((uintptr_t)pa & ~0xfff) | 0x1; 
