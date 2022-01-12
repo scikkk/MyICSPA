68b9ce15dc5b661323819dc7eda29c19296d279b
tracer-ics2021 <tracer@njuics.org>
2022-01-12 20:39:45 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 398389e2707542e8ae81d9e07078b481     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  20:39:45 up 26 min,  1 user,  load average: 0.29, 0.20, 0.12

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -58,7 +58,7 @@ Context* schedule(Context *prev) {
 	printf("current=%p\tcurrent->as=%p\tas.ptr=%p\tpdir=%p\n", current, current->as, current->as.ptr, current->cp->pdir);
 	// always select pcb[0] as the new process
 	current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-	printf("current=%p\tcurrent->as=%p\tas.ptr=%p\tpdir=%p\n", current, current->as, current->as.ptr, current->cp->pdir);
+	printf("current=%p\tcurrent->as=%p\tas.ptr=%p\tpdir=%p\tmaxbrk=%p\n", current, current->as, current->as.ptr, current->cp->pdir, current->max_brk);
 	
 	// then return the new context
 	/* printf("cur-cp=%p\n", current->cp); */
