855a3201aeffc33739ac5a406e5d0036f231b02f
tracer-ics2021 <tracer@njuics.org>
2022-01-12 13:07:35 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d41388a34b844a80bbb0addf2c91ffd5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  13:07:35 up  2:33,  1 user,  load average: 0.02, 0.10, 0.13

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -8,6 +8,7 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 word_t vaddr_read(vaddr_t addr, int len) {
 	switch(isa_mmu_check(addr, len, 0)){
 		case MMU_TRANSLATE:
+	printf("satp = %p\n", cpu.satp);
 			addr = isa_mmu_translate(addr, len, 0);
 		case MMU_DIRECT:
 			break;
@@ -19,6 +20,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 void vaddr_write(vaddr_t addr, int len, word_t data) {
 	switch(isa_mmu_check(addr, len, 0)){
 		case MMU_TRANSLATE:
+	printf("satp = %p\n", cpu.satp);
 			addr = isa_mmu_translate(addr, len, 0);
 		case MMU_DIRECT:
 			paddr_write(addr, len, data);
