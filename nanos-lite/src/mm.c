0b1584d9fc49e0cb562e4d003ebf047289541ebe
tracer-ics2021 <tracer@njuics.org>
2022-01-12 18:53:28 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: a7abcb5e18eb442bae0cdb51b7ad4795     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  18:53:28 up  2:28,  1 user,  load average: 0.06, 0.08, 0.08

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -24,7 +24,7 @@ void free_page(void *p) {
 #include <proc.h>
 int mm_brk(uintptr_t brk) {
 	if(brk > current->max_brk){
-		printf("brk increase: %p --> %p\n", current->max_brk, brk);
+		printf("pcb=%p\tbrk increase: %p --> %p\n", current, current->max_brk, brk);
 		while(brk > current->max_brk){
 			uintptr_t page_begin  = (uintptr_t)new_page(1);                                                                                                      
 			/* map(&current->as , (void*)(brk&~0xfff), (void*)page_begin, 0); */
