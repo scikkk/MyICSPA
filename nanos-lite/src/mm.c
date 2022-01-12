83aa1f023f7e1b07d2368394d5651d932dd0fd5c
tracer-ics2021 <tracer@njuics.org>
2022-01-12 18:17:08 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: a7abcb5e18eb442bae0cdb51b7ad4795     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  18:17:08 up  1:52,  1 user,  load average: 0.03, 0.02, 0.03

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -3,33 +3,41 @@
 static void *pf = NULL;
 
 void* new_page(size_t nr_page) {
-  void* ret = pf;
-  pf += nr_page*4096;
-  return ret;
+	void* ret = pf;
+	pf += nr_page*4096;
+	return ret;
 }
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-  void* ret = new_page(n >> 12);
-  memset(ret, 0, n);
-  return ret;
+	void* ret = new_page(n >> 12);
+	memset(ret, 0, n);
+	return ret;
 }
 #endif
 
 void free_page(void *p) {
-  panic("not implement yet");
+	panic("not implement yet");
 }
 
 /* The brk() system call handler. */
+#include <proc.h>
 int mm_brk(uintptr_t brk) {
-  return 0;
+	if(brk > current->max_brk){
+		while(brk > current->max_brk){
+			uintptr_t page_begin  = (uintptr_t)new_page(1);                                                                                                      
+			map(&current->as , (void*)brk, (void*)page_begin, 0);
+		}
+		current->max_brk = brk;
+	}
+	return 0;
 }
 
 void init_mm() {
-  pf = (void *)ROUNDUP(heap.start, PGSIZE);
-  Log("free physical pages starting from %p", pf);
+	pf = (void *)ROUNDUP(heap.start, PGSIZE);
+	Log("free physical pages starting from %p", pf);
 
 #ifdef HAS_VME
-  vme_init(pg_alloc, free_page);
+	vme_init(pg_alloc, free_page);
 #endif
 }
