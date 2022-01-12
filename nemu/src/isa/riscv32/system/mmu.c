5a9150cb7452bb07a3b658088025f3010ca6f3f3
tracer-ics2021 <tracer@njuics.org>
2022-01-12 17:18:58 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: a7abcb5e18eb442bae0cdb51b7ad4795     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  17:18:58 up  1:05,  1 user,  load average: 0.34, 0.40, 0.26

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -27,13 +27,16 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 	paddr_t pte2_addr = (pte1 & ~0xfff) | (vpn0 << 2);
 	paddr_t pte2 = paddr_read(pte2_addr, 4);
 	if((pte2 & 0x1) == 0){
-		printf("pte2=0x%x\n", pte2);
+		printf("mmu vaddr=0x%x\n", vaddr);
+		printf("mmu satp=0x%x\n", cpu.satp);
+		printf("mmu pte2_addr=0x%x\n", pte2_addr);
+		printf("mmu pte2=0x%x\n", pte2);
 	}
 	assert((pte2 & 0x1) == 1);
 	paddr = (pte2 & ~0xfff) | (vaddr & 0xfff); 
-	if(paddr != vaddr){
-		printf("paddr=0x%x\tvaddr=0x%x\n", paddr, vaddr);
-	}
+	/* if(paddr != vaddr){ */
+	/* 	printf("paddr=0x%x\tvaddr=0x%x\n", paddr, vaddr); */
+	/* } */
 	/* assert(paddr == vaddr); */
 	return paddr;
 	/* return MEM_RET_FAIL; */
