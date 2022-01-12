512836056e124e04bafdae91c9e5a77904e03d76
tracer-ics2021 <tracer@njuics.org>
2022-01-12 14:30:07 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d41388a34b844a80bbb0addf2c91ffd5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  14:30:07 up  3:56,  1 user,  load average: 0.12, 0.22, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -96,14 +96,14 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 	Area kstack = {pcb, pcb+1};
 	printf("as.area.start=%p\tend=%p\n", pcb->as.area.start, pcb->as.area.end);
 	uintptr_t gprx = (uintptr_t)new_page(8);
-	map(&pcb->as , (void*)gprx         , (void*)pcb->as.area.end - 0x8000, 0);
-	map(&pcb->as , (void*)gprx + 0x1000, (void*)pcb->as.area.end - 0x7000, 0);
-	map(&pcb->as , (void*)gprx + 0x2000, (void*)pcb->as.area.end - 0x6000, 0);
-	map(&pcb->as , (void*)gprx + 0x3000, (void*)pcb->as.area.end - 0x5000, 0);
-	map(&pcb->as , (void*)gprx + 0x4000, (void*)pcb->as.area.end - 0x4000, 0);
-	map(&pcb->as , (void*)gprx + 0x5000, (void*)pcb->as.area.end - 0x3000, 0);
-	map(&pcb->as , (void*)gprx + 0x6000, (void*)pcb->as.area.end - 0x2000, 0);
-	map(&pcb->as , (void*)gprx + 0x7000, (void*)pcb->as.area.end - 0x1000, 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x8000, (void*)gprx         , 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x7000, (void*)gprx + 0x1000, 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x6000, (void*)gprx + 0x2000, 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x5000, (void*)gprx + 0x3000, 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x4000, (void*)gprx + 0x4000, 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x3000, (void*)gprx + 0x5000, 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x2000, (void*)gprx + 0x6000, 0);
+	map(&pcb->as , (void*)pcb->as.area.end - 0x1000, (void*)gprx + 0x7000, 0);
 	int argc = 0, envpc = 0;
 	if(argv){
 		argc = -1;
