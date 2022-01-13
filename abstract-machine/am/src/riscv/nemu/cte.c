680149f7f781f5cdcd65b0e9769a0b62195edb6f
tracer-ics2021 <tracer@njuics.org>
2022-01-13 14:04:07 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 154ec64645ca44f9a81690f44c0a1ff7     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  14:04:07 up 37 min,  1 user,  load average: 0.27, 0.20, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -47,12 +47,12 @@ Context* __am_irq_handle(Context *c) {
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
-	printf("user_handler=%p\thandler=%p\n", user_handler, handler);
+	/* printf("user_handler=%p\thandler=%p\n", user_handler, handler); */
 	// initialize exception entry
 	asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
 	// register event handler
-	printf("user_handler=%p\thandler=%p\n", user_handler, handler);
+	/* printf("user_handler=%p\thandler=%p\n", user_handler, handler); */
 	assert(user_handler == NULL);
 	user_handler = handler;
 
