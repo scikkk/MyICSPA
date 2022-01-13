46cb4a7a26d1434213bfa7700974424b17ad5f4a
tracer-ics2021 <tracer@njuics.org>
2022-01-13 15:32:39 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 154ec64645ca44f9a81690f44c0a1ff7     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  15:32:40 up  2:06,  1 user,  load average: 0.13, 0.24, 0.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,8 +32,8 @@ void init_proc() {
 	/* char *argv[] = {"--skip", NULL}; */
 	/* context_uload(&pcb[1], "/bin/pal", argv, NULL); */
 
-	/* char *argv[] = {"/bin/exec-test", NULL}; */
-	/* context_uload(&pcb[1], "/bin/exec-test", argv, NULL); */
+	char *argv[] = {"/bin/exec-test", NULL};
+	context_uload(&pcb[1], "/bin/exec-test", argv, NULL);
 	
 	/* context_uload(&pcb[1], "/bin/menu", NULL, NULL); */
 
@@ -43,7 +43,7 @@ void init_proc() {
 
 	/* context_uload(&pcb[1], "/bin/dummy", NULL, NULL); */
 
-	context_uload(&pcb[1], "/bin/hello", NULL, NULL);
+	/* context_uload(&pcb[1], "/bin/hello", NULL, NULL); */
 
 	/* context_uload(&pcb[1], "/bin/nslider", NULL, NULL); */
 
