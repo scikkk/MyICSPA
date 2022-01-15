b30f95c93dedd5e4fe9a1fb07407ac907b0c6aca
tracer-ics2021 <tracer@njuics.org>
2022-01-15 10:51:21 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 0648582a7af14e1b9b6b238b0f9d952e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  10:51:21 up 7 min,  1 user,  load average: 0.26, 0.29, 0.18

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main() {
+int main(int argc, char** argv) {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
   while (1) {
     j ++;
     if (j == 999999) {
-		printf("argc=%d\targv[0]=%x\targv[1]=%x\n", argc, argv[0], argv[1]);
+		printf("argc=%d\targv[0]=%s\targv[1]=%s\n", argc, argv[0], argv[1]);
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
       j = 0;
     }
