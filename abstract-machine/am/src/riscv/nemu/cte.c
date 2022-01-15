9869e7d5de49d33f3eef32436aa9c3aeb01b4d38
tracer-ics2021 <tracer@njuics.org>
2022-01-15 13:25:45 +0800

    >  compile 201300008    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 0648582a7af14e1b9b6b238b0f9d952e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  13:25:45 up  2:13,  1 user,  load average: 0.01, 0.06, 0.15

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,29 +13,21 @@ Context* __am_irq_handle(Context *c) {
 	if (user_handler) {
 		Event ev = {0};
 		/* ev.event = c->mcause; */
-		/* switch (c->mcause) { */
-		/* 	case EVENT_NULL: ev.event = EVENT_NULL;break; */
-		/* 	case EVENT_YIELD: ev.event = EVENT_YIELD; break; */
-		/* 	case EVENT_SYSCALL:ev.event = EVENT_SYSCALL; break; */
-		/* 	case EVENT_PAGEFAULT:ev.event = EVENT_PAGEFAULT;break; */
-		/* 	case EVENT_ERROR:ev.event=EVENT_ERROR;break; */
-		/* 	case EVENT_IRQ_TIMER :ev.event=EVENT_IRQ_TIMER;break; */
-		/* 	case EVENT_IRQ_IODEV:ev.event=EVENT_IRQ_IODEV;break; */
-		/* 	default: printf("Unkonwn error num:%d\n", c->mcause); assert(0); */
-		/* } */
-		/* printf("x0:%d\n",c->gpr[0]); */
-		/* printf("mstatus:%d\nmepc:%d\nmcause:%d\n", c->mstatus,c->mepc,c->mcause); */
-
-		/* printf("__am_irq_handle=%p\n", c); */
-		/* if(c->mcause != 11) */ 
-		switch(c->GPR1){
-			case 0:case 1:case 2:case 3: case 4:case 5: case 6:case 7:
-			case 8:case 9:case 10:case 11:case 12: case 13:case 14: case 15: case 16: case 17:
-			case 18: case 19: ev.event = EVENT_SYSCALL;break;
-			default: ev.event = EVENT_YIELD;
+		switch (c->mcause) {
+			case EVENT_SYSCALL:ev.event = EVENT_SYSCALL;
+							  if(c->GPR1 == -1) ev.event = EVENT_YIELD;
+							   break;
+			case 0x80000007 :ev.event=EVENT_IRQ_TIMER;break;
+			default: printf("Unkonwn error num:%d\n", c->mcause); assert(0);
 		}
-		if(c->mcause == 0x80000007) 
-			ev.event = EVENT_IRQ_TIMER;
+		/* switch(c->GPR1){ */
+		/* 	case 0:case 1:case 2:case 3: case 4:case 5: case 6:case 7: */
+		/* 	case 8:case 9:case 10:case 11:case 12: case 13:case 14: case 15: case 16: case 17: */
+		/* 	case 18: case 19: ev.event = EVENT_SYSCALL;break; */
+		/* 	default: ev.event = EVENT_YIELD; */
+		/* } */
+		/* if(c->mcause == 0x80000007) */ 
+		/* 	ev.event = EVENT_IRQ_TIMER; */
 		c = user_handler(ev, c);
 		assert(c != NULL);
 		/* assert(0); */
